37ebca2678f7abf7176a332790b4f1d16a6ea8d6
Elena Cazan <cazan.elena23@yahoo.com>
2023-11-23 11:54:29 +0000

    upload image function

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
-import React from "react";
 import { RetreiveFromSupabase } from "@/utils/supabase/RetreiveFromSupabase";
 
 export default async function Index() {
   const cookieStore = cookies();
-
   const canInitSupabaseClient = () => {
     // This function is just for the interactive tutorial.
     // Feel free to remove it once you have Supabase connected.
