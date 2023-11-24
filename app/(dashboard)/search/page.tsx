e0aa5a9a2040ec9259785be2b563aa2fe734d5da
Elena Cazan <cazan.elena23@yahoo.com>
2023-11-24 14:11:53 +0000

    clean up code, added some item details to be displayed

--- app/(dashboard)/search/page.tsx
+++ app/(dashboard)/search/page.tsx
@@ -2,59 +2,38 @@
 
 import Search from '@/components/form/Search';
 import searchItem from '@/utils/supabase/searchByName';
+import { item } from '@/utils/supabase/types';
 import { useState } from 'react';
+
 export default function SearchItem() {
-  const [searchResults, setSearchResults] = useState([]);
-  // const [categories, setCategories] = useState([]);
+  const [searchResults, setSearchResults] = useState<item[]>();
 
   async function handleSearch(term: string) {
     const items = await searchItem(term);
 
-    // await Promise.all(
-    //   items.map(async (item) => {
-    //     item.image = await fetchImage(item.image_id);
-    //   })
-    // );
-
     setSearchResults(items);
   }
-
-  console.log(searchResults);
-  // async function fetchCategoriesData() {
-  //   const categoriesData = await fetchCategories();
-  //   setCategories(categoriesData);
-  // }
-
-  // useEffect(() => {
-  //   fetchCategoriesData();
-  // }, []);
 
   return (
     <>
       <Search onSearch={handleSearch} />{' '}
-      {/* {searchResults.length === 0 && (
-        <div className='grid grid-cols-2 gap-4'>
-          {categories.map((category) => (
+      <div className='mt-4'>
+        {searchResults &&
+          searchResults.map((result) => (
             <div
-              key={category.category_name}
-              className='bg-white shadow-md rounded-md p-4'
+              className='bg-white p-2 border rounded shadow mb-2'
+              key={result.id}
             >
-              <Link href={`/products/${category.category_name}`}>
-                <h1 className='text-2xl font-bold'>{category.category_name}</h1>
-              </Link>
+              <p>{result.item_name}</p>
+              <p>{result.brand}</p>
+              <p>{result.condition}</p>
+              <img
+                src='tobe added'
+                alt='picture'
+                className='w-32 h-32 object-contain'
+              />
             </div>
           ))}
-        </div>
-      )} */}
-      <div className='mt-4'>
-        {searchResults.map((result) => (
-          <div
-            className='bg-white p-2 border rounded shadow mb-2'
-            key={result.id}
-          >
-            <img src='tobe added' alt={} className='w-32 h-32 object-contain' />
-          </div>
-        ))}
       </div>
     </>
   );
