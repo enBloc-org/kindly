9db00f13886ccc4d4506fc677bf5b4d8f9f63a69
ShaughnAnderson94 <andersji94@gmail.com>
2023-11-24 11:58:34 +0000

    Finished delete item and cleaned up other supabase functions

--- utils/supabase/DeleteTableRow.ts
+++
@@ -1,1 +0,0 @@
-// note for if we are turning row level security back on and this breaks If you use delete() with filters and you have RLS enabled, only rows visible through SELECT policies are deleted. Note that by default no rows are visible, so you need at least one SELECT/ALL policy that makes the rows visible.
