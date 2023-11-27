cecc22e141217266b5d8c67a17d0c6584eec887a
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-11-27 16:05:39 +0000

    Insert Retrieve function

--- app/(dashboard)/my-items/page.tsx
+++ app/(dashboard)/my-items/page.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+// import ItemCard from '@/components/ItemCard';
+import { RetreiveItemsFromSupabase } from '@/utils/supabase/RetreiveFromSupabase';
 
-const MyItemsPage = () => {
+const MyItemsPage = async () => {
+  const items = await RetreiveItemsFromSupabase('items', '', '', '');
+  console.log(items);
   return <div>MyItemsPage</div>;
 };
 
