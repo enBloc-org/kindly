c521bdb6441852670f073cffb6b1d854d291e6fd
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-11-27 16:22:58 +0000

    Add hardcoded id to display items.working

--- app/(dashboard)/my-items/page.tsx
+++ app/(dashboard)/my-items/page.tsx
@@ -3,7 +3,13 @@ import React from 'react';
 import { RetreiveItemsFromSupabase } from '@/utils/supabase/RetreiveFromSupabase';
 
 const MyItemsPage = async () => {
-  const items = await RetreiveItemsFromSupabase('items', '', '', '');
+  const myid = 'b6e3f949-e931-453b-a3c8-afa5159b0034';
+  const items = await RetreiveItemsFromSupabase(
+    'items',
+    '',
+    'donated_by',
+    myid
+  );
   console.log(items);
   return <div>MyItemsPage</div>;
 };
