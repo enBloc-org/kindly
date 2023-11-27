6834e95541d7272a5434ecece77242bacd942f7c
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-11-27 16:41:22 +0000

    Fix with right user id.Typescript error to be fixed

--- app/(dashboard)/my-items/page.tsx
+++ app/(dashboard)/my-items/page.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 // import ItemCard from '@/components/ItemCard';
 import { RetreiveItemsFromSupabase } from '@/utils/supabase/RetreiveFromSupabase';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
 
 const MyItemsPage = async () => {
-  const myid = 'b6e3f949-e931-453b-a3c8-afa5159b0034';
+  const supabase = createServerComponentClient({ cookies });
+  const { data } = await supabase.auth.getSession();
+  console.log(data.session?.user.id);
+  const currentUser = data.session?.user.id;
+
   const items = await RetreiveItemsFromSupabase(
     'items',
     '',
     'donated_by',
-    myid
+    currentUser
   );
   console.log(items);
   return <div>MyItemsPage</div>;
