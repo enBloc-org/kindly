afdc694e9ddc409c85d81662fde976cd08ad1e8c
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-11-27 17:20:39 +0000

    all props undefined

--- app/(dashboard)/my-items/page.tsx
+++ app/(dashboard)/my-items/page.tsx
@@ -3,12 +3,19 @@ import React from 'react';
 import { RetreiveItemsFromSupabase } from '@/utils/supabase/RetreiveFromSupabase';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
+import ItemCard from '@/components/ItemCard';
 
 const MyItemsPage = async () => {
   const supabase = createServerComponentClient({ cookies });
   const { data } = await supabase.auth.getSession();
-  console.log(data.session?.user.id);
+
   const currentUser = data.session?.user.id;
+
+  if (!currentUser) {
+    // Handle the case where the user ID is not available
+    console.error('User ID not available');
+    return <div>Error loading items</div>;
+  }
 
   const items = await RetreiveItemsFromSupabase(
     'items',
@@ -16,8 +23,24 @@ const MyItemsPage = async () => {
     'donated_by',
     currentUser
   );
-  console.log(items);
-  return <div>MyItemsPage</div>;
+
+  return (
+    <ul>
+      {items?.map((item) => (
+        <li key={item.id}>
+          <ItemCard
+            img={item.image}
+            title={item.item_name}
+            size={item.size}
+            donor={item.donated_by}
+            location={item.postcode}
+            postageCovered={item.postable}
+            link={item.item_description}
+          ></ItemCard>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default MyItemsPage;
