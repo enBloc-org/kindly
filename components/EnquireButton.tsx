d447687985d7e0a83d6f7198de6e8c5bce785be2
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2023-11-29 12:50:51 +0000

    Hotfix of merge conflicts and button disabling and error handling for send message button

--- components/EnquireButton.tsx
+++ components/EnquireButton.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { SyntheticEvent, useEffect, useState } from 'react';
 
 export interface EnquireButtonProps {
   donorEmail: string;
@@ -6,40 +7,68 @@ export interface EnquireButtonProps {
   title: string;
 }
 
-import { SyntheticEvent } from 'react';
-
-export default async function EnquireButton({
+export default function EnquireButton({
   donorEmail,
   userEmail,
   title,
 }: EnquireButtonProps) {
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [messageSuccess, setMessageSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
   const subject = title;
   const message = `a user with email ${userEmail} is interested in your item`;
 
-  const sendMail = async (e: SyntheticEvent) => {
-    e.preventDefault();
-
-    const response = await fetch('/api/sendEmail', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        subject,
-        message,
-        donorEmail,
-      }),
-    });
-    if (!response.ok) {
-      // Check for non-2xx HTTP status codes
-      throw new Error(`HTTP error! Status: ${response.status}`);
+  useEffect(() => {
+    if (isDisabled) {
+      const sendMail = async () => {
+        try {
+          const response = await fetch('/api/sendEmail', {
+            method: 'POST',
+            headers: {
+              'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+              subject,
+              message,
+              donorEmail,
+            }),
+          });
+          if (!response.ok) {
+            setIsDisabled(false);
+            setError(true);
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          setMessageSuccess(true);
+          setError(false);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      sendMail();
     }
+  }, [isDisabled]);
+
+  const sumbitHandler = (e: SyntheticEvent) => {
+    e.preventDefault();
+    setIsDisabled(true);
   };
+
   return (
-    <form onSubmit={sendMail}>
-      <button type='submit' className='button button-rounded'>
-        <span>SEND MESSAGE</span>
+    <form className='flex flex-col items-center gap-5' onSubmit={sumbitHandler}>
+      <button
+        type='submit'
+        className='button button-rounded disabled:bg-primaryGray'
+        disabled={isDisabled}
+      >
+        <span>{messageSuccess ? 'MESSAGE SENT' : 'SEND MESSAGE'}</span>
       </button>
+      {error && (
+        <div className='text-primaryOrange text-center'>
+          <p>There has been an error sending you message.</p>
+          <p>Please try again.</p>
+        </div>
+      )}
     </form>
   );
 }
