d9bff1ae3dee481eeda53b61856ecb342b4a1660
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-11-29 16:27:16 +0000

    Add refresh router when delete Button is clicked

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,10 +1,17 @@
 'use client';
 import deleteItem from '@/utils/supabase/DeleteItem';
+import { useRouter } from 'next/navigation';
 
 interface DeleteButtonProps {
   itemId: number;
 }
 
 export default function DeleteButton({ itemId }: DeleteButtonProps) {
-  return <button onClick={() => deleteItem(itemId)}>Delete</button>;
+  // Get id of items you want to delete from database and refresh page
+  const router = useRouter();
+  return (
+    <button onClick={() => deleteItem(itemId).then(router.refresh)}>
+      Delete
+    </button>
+  );
 }
