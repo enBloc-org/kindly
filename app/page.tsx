8424d1367196807548ea35edfe348db9082e91ca
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-11-29 12:05:30 +0000

    add correct redirect after logging in and basic structure of landing-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 export default async function Index() {
   return (
     <div>
-      <h1>Kindly</h1>
+      <header className=' bg-background flex justify-center py-2   '>
+        <Image
+          src='/KINDLY_LOGO.png'
+          alt='Kindly Logo'
+          height={308}
+          width={129}
+        />
+      </header>
+      <h1>A place to support Ukrainian refugees</h1>
+      <p>I would like to donate items</p>
+      <Link href='/signup'>Register here</Link>
+      <p>I’m a Ukrainian refugee and I would like to receive or donate items</p>
+      <div>
+        <p>Already have an account?</p>
+        <Link href='/login'>Log in</Link>
+      </div>
     </div>
   );
 }
