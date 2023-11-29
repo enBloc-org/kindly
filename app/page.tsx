cc71e2db535d16ec0bc590f198a7ea89a2f12cf5
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-11-29 12:40:15 +0000

    Add vectors and change font size REGISTER HERE

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,55 @@ export default async function Index() {
         />
       </header>
       <h1 className='italic'>A place to support Ukrainian refugees</h1>
-      <p className='font-extrabold'>I would like to donate items</p>
-      <Link href='/signup' className='font-extrabold italic text-primaryOrange'>
+      <div className='flex'>
+        <p className='font-extrabold'>I would like to donate items</p>
+        <svg
+          width='88'
+          height='130'
+          viewBox='0 0 88 130'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            d='M2 11.4408C115.392 -13.8709 102.294 17.8254 27 105.441'
+            stroke='#BBE3C3'
+            strokeWidth='5'
+          />
+          <path
+            d='M10.8446 115.424L22.0551 87.2624L40.8385 111.052L10.8446 115.424Z'
+            fill='#BBE3C3'
+          />
+        </svg>
+      </div>
+      <Link
+        href='/signup'
+        className='font-extrabold text-3xl italic text-primaryOrange'
+      >
         Register here
       </Link>
-      <p className='font-extrabold text-center'>
-        I’m a Ukrainian refugee and I would like to receive or donate items
-      </p>
+      <div>
+        <svg
+          width='115'
+          height='165'
+          viewBox='0 0 115 165'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            d='M85.5 160.5C-33.984 170.766 -17.7625 132.009 96 17.5'
+            stroke='#BBE3C3'
+            strokeWidth='5'
+          />
+          <path
+            d='M103.81 14.416L92.1209 42.6195L73.4517 17.6502L103.81 14.416Z'
+            fill='#BBE3C3'
+          />
+        </svg>
+        <p className='font-extrabold text-center'>
+          I’m a Ukrainian refugee and I would like to receive or donate items
+        </p>
+      </div>
+
       <div className='flex space-x-2'>
         <p>Already have an account?</p>
         <Link href='/login' className='font-extrabold text-primaryOrange'>
