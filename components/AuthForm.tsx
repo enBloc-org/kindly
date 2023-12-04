31675558e28e4c78584d6dba1c50f87411dbaf78
Elena Cazan <cazan.elena23@yahoo.com>
2023-12-04 12:06:36 +0000

    fixed log in error

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,7 +20,7 @@ const AuthForm: React.FC<AuthFormProps> = ({
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (!isEmailAgreed) {
+    if (isSignUp && !isEmailAgreed) {
       setErrorMessage('Please agree to share your email address.');
       return;
     }
