6244287505104cb0ddd4e9b95533de1a76b1681d
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-12-04 15:17:06 +0000

    Finish basic structure

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -10,7 +10,20 @@ const Modal = ({ name }) => {
 
   return (
     <>
-      <button onClick={toggleModal}>{name}</button>;
+      <button className='button button-rounded' onClick={toggleModal}>
+        {name}
+      </button>
+      {modal && (
+        <div className='modal'>
+          <div className='overlay' onClick={toggleModal}></div>
+          <div className='modal-content'>
+            <h1>Warning</h1>
+            <p>By pressing `confirm` again you will delete X</p>
+          </div>
+          <button>Confirm</button>
+          <button onClick={toggleModal}>Cancel</button>
+        </div>
+      )}
     </>
   );
 };
