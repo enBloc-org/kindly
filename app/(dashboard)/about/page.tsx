f4e3c8d1012d5d42f2da09c9d0dc25decacfbba7
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2023-12-04 17:45:28 +0000

    Text formatting on home and about page, media queries on the same
    Relates #155

--- app/(dashboard)/about/page.tsx
+++ app/(dashboard)/about/page.tsx
@@ -7,15 +7,17 @@ import questionsAndAnswers from '@/utils/faqsArray';
 
 const AboutPage = () => {
   return (
-    <div className='p-6'>
-      <h1 className='font-extrabold text-4xl text-center'>About</h1>
-      <div className='my-5 text-justify'>
+    <div className='flex flex-col items-center justify-center gap-10 px-10 mt-10 lg:text-lg lg:gap-20'>
+      <h1 className='font-extrabold text-4xl p-10 text-center'>About</h1>
+      <p className='lg:px-96'>
         KINDLY is a platform that lets you donate unwanted items to Ukrainian
         refugees. Donors can upload unwanted clothes, shoes, toys, books and
         household items. Your generous contributions will help adults and
         children who’ve fled the war in Ukraine. In the future we hope to expand
-        KINDLY to serve the wider refugee community. KINDLY is a sister project
-        to{' '}
+        KINDLY to serve the wider refugee community.
+      </p>
+      <p className='lg:px-96'>
+        KINDLY is a sister project to{' '}
         <Link
           className='text-primaryOrange font-extrabold'
           href='https://linktr.ee/trafalgargirls'
@@ -31,24 +33,25 @@ const AboutPage = () => {
         >
           Tech for Better programme
         </Link>{' '}
-        by <span className='font-extrabold'>Founders and Coders</span>. You can
-        contact KINDLY via email (xxx insert email when available) Follow the
-        KINDLY journey on Instagram, Facebook, Threads (icons, hyperlinks will
-        be available at later date xxx){' '}
-        <div className='flex justify-center my-5'>
-          <Link href=''>
-            <FaInstagramSquare />
-          </Link>
-          <Link href=''>
-            <FaSquareThreads />
-          </Link>
-          <Link href=''>
-            <FaTwitter />
-          </Link>
-        </div>
+        by <span className='font-extrabold'>Founders and Coders</span>.
+      </p>
+      <p className='lg:px-96'>
+        You can contact KINDLY via email (xxx insert email when available)
+        Follow the KINDLY journey on Instagram, Facebook, Threads.
+      </p>
+      <div className='flex justify-center gap-14 my-5'>
+        <Link href=''>
+          <FaInstagramSquare size={60} />
+        </Link>
+        <Link href=''>
+          <FaSquareThreads size={60} />
+        </Link>
+        <Link href=''>
+          <FaTwitter size={60} />
+        </Link>
       </div>
       <div>
-        <h1 className='font-extrabold text-4xl text-center'>FAQs</h1>
+        <h2 className='font-extrabold text-4xl text-center mt-8'>FAQs</h2>
         {questionsAndAnswers.map((faq, index) => (
           <FAQItem key={index} {...faq}></FAQItem>
         ))}
