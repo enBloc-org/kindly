2805c46f4fc3b0855dc32093b63332df3a4d7a8d
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2023-12-05 15:40:39 +0000

    Hot fix for conditional rendering of header and footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,23 +1,13 @@
 'use client';
-import { useState, useEffect } from 'react';
 import MobileNavbar from './MobileNavbar';
+import useMediaQuery from './hooks/useMediaQuery';
 
 const Footer = () => {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => setWindowWidth(window.innerWidth);
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+  const isBreakpoint = useMediaQuery(1000);
 
   return (
     <footer className='sticky bottom-0 bg-background py-1'>
-      {windowWidth < 1000 && <MobileNavbar />}
+      {isBreakpoint && <MobileNavbar />}
     </footer>
   );
 };
---
+++ components/hooks/useMediaQuery.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect, useCallback } from 'react';
+
+const useMediaQuery = (width: number) => {
+  const [targetReached, setTargetReached] = useState(false);
+
+  const updateTarget = useCallback((e: MediaQueryListEvent) => {
+    if (e.matches) {
+      setTargetReached(true);
+    } else {
+      setTargetReached(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    const media = window.matchMedia(`(max-width: ${width}px)`);
+    media.addListener(updateTarget);
+
+    // Check on mount (callback is not called until a change occurs)
+    if (media.matches) {
+      setTargetReached(true);
+    }
+
+    return () => media.removeListener(updateTarget);
+  }, []);
+
+  return targetReached;
+};
+
+export default useMediaQuery;
