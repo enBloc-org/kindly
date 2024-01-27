509738eb6255379facde4675b963583509c53eb7
Beth <bethazz@hotmail.co.uk>
2024-01-27 12:01:36 +0000

    rename EditSupabaseRow to editRow and update imports

--- utils/supabase/editRow.ts
+++ utils/supabase/editRow.ts
@@ -1,7 +1,7 @@
 import newClient from '@/config/supabaseclient';
 import { editProfile, item, profile, updateReserved } from './types';
 
-export default async function EditSupabaseRow(
+export default async function editRow(
   table: string,
   EditValues: item | profile | editProfile | updateReserved,
   columnName: string,
@@ -11,5 +11,5 @@ export default async function EditSupabaseRow(
   await supabase.from(table).update(EditValues).eq(columnName, columnCriteria);
   // use example
   // we want to edit the item with id=5 from the item table we want to replace its update its brand and its requested by to do
-  //that we would use the following EditSupabaseRow(item,{brand: 'Zara',requested_by:'Hank' }, 'id', 5)
+  //that we would use the following editRow(item,{brand: 'Zara',requested_by:'Hank' }, 'id', 5)
 }
