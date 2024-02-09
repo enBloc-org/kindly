ac06cdaff1209307ee5636da4b4ff91b6d59bfcb
camelPhonso <114600712+camelPhonso@users.noreply.github.com>
2024-02-09 12:25:03 +0000

    installed playwright-ct for component testing

--- tests/MessageCard.spec.tsx
+++ tests/MessageCard.spec.tsx
@@ -1,27 +1,26 @@
 import { test, expect } from '@playwright/experimental-ct-react';
 
-import MessageCard from '@/components/messaging/MessageCard';
+import MessageCard from '../components/messaging/MessageCard';
 
 const testMessage = {
-  id: 1,
-  created_at: new Date(),
+  sender_id: '1',
+  created_at: new Date().toString(),
   conversation_id: 45,
-  sender_id: '666',
   message_text:
     'Hi! I really like your stuff and I would like to have it please!',
   is_read: false,
+  currentUser: '1',
 };
 
 test.describe('MessageCard component', () => {
   test('renders', async ({ mount }) => {
     const component = await mount(
       <MessageCard
-        id={testMessage.id}
-        created_at={testMessage.created_at}
-        conversation_id={testMessage.conversation_id}
         sender_id={testMessage.sender_id}
+        created_at={testMessage.created_at}
         message_text={testMessage.message_text}
         is_read={testMessage.is_read}
+        currentUser={testMessage.currentUser}
       />
     );
 
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { test } from '@playwright/experimental-ct-react';
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:3000/login');
