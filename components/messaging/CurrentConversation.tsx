ff749ce6da0d037050dcc97b2cf69239e83276c2
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2024-02-14 13:49:04 +0100

    Useeffect to set up supabase realtime on currentConversation. Currently just console logging resulting payload
    Relates #34

--- components/messaging/CurrentConversation.tsx
+++ components/messaging/CurrentConversation.tsx
@@ -5,8 +5,11 @@ import MessageCard from './MessageCard';
 import MessageForm from './MessageForm';
 import { useContext, useEffect } from 'react';
 import useConversation from '../../app/(dashboard)/conversations/useConversation';
+import { createSupabaseClient } from '@/utils/supabase/supabaseClient';
 
 const CurrentConversation: React.FC = () => {
+  const supabase = createSupabaseClient;
+
   const { allConversations, currentConversation, setCurrentConversation } =
     useContext(useConversation);
 
@@ -22,6 +25,31 @@ const CurrentConversation: React.FC = () => {
         )[0]
       );
   }, []);
+
+  useEffect(() => {
+    const channel = supabase
+      .channel('realtime messages')
+      .on(
+        'postgres_changes',
+        {
+          event: 'INSERT',
+          schema: 'public',
+          table: 'messages',
+        },
+        (payload) => {
+          // setCurrentConversation(curr[
+          //   ...currentConversation,
+          //   payload.new as MessageType,
+          // ]);
+          console.log(payload.new);
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [supabase]);
 
   return (
     <div className='flex w-2/4 flex-col'>
