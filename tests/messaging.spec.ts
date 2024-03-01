ae3cc6d1be223d4f5ead5dd1169a2dc7284291cc
Beth <bethazz@hotmail.co.uk>
2024-03-01 10:22:59 +0000

    remove messaging end-to-end test from this pr

--- tests/messaging.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/experimental-ct-react';
-
-test('test messaging feature', async ({ page }) => {
-    console.log("here")
-
-  await page.goto('http://localhost:3000/conversations');
-  await page.getByPlaceholder('Type your message here').fill('Hello please work');
-});
