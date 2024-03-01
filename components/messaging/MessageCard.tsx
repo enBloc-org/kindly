08284ed1864bce601f0c10dbcd4459632a051462
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2024-03-01 11:51:50 +0000

    Large screen styling for Message Card
    Relates #76

--- components/messaging/MessageCard.tsx
+++ components/messaging/MessageCard.tsx
@@ -23,13 +23,19 @@ const MessageCard: React.FC<MessageCardProps> = ({
 
   return (
     <div
-      className={`message-card ${isCurrentUser ? 'float-right bg-secondaryGray' : 'float-left bg-secondaryGreen'} my-2`}
+      className={`message-card ${isCurrentUser ? 'float-right bg-secondaryGray lg:left-24' : 'float-left bg-secondaryGreen lg:right-10'} my-2`}
     >
       <div className='mx-2 flex items-center justify-center'>
-        <p className='text-lg'>{message_text}</p>
+        <p
+          className={`px-2 py-2 text-lg ${message_text.length > 50 ? 'md:px-4' : ''} ${isCurrentUser ? '' : 'lg:ml-10'}`}
+        >
+          {message_text}
+        </p>
       </div>
-      <div className='flex flex-1 flex-col items-end justify-between gap-2'>
-        <p className='text-sm font-light'>13:40</p>
+      <div
+        className={`flex flex-1 flex-col items-end justify-between gap-2 ${isCurrentUser ? 'lg:mr-24' : ''}`}
+      >
+        <p className='text-sm font-light lg:text-base'>13:40</p>
         <TickIcon read={is_read} />
       </div>
     </div>
