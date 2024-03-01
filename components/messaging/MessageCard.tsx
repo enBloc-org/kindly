b436abc16c70d8f7076d1694ed0f16e323c2fb8e
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2024-03-01 15:33:14 +0000

    Refectored uneccessary ternaries and fixed margin on large screen
    Relates #76

--- components/messaging/MessageCard.tsx
+++ components/messaging/MessageCard.tsx
@@ -29,16 +29,16 @@ const MessageCard: React.FC<MessageCardProps> = ({
     >
       <div className='mx-2 flex items-center justify-center'>
         <p
-          className={`px-2 py-2 text-lg ${message_text.length > 50 ? 'md:px-4' : ''} ${isCurrentUser ? '' : 'lg:ml-10'}`}
+          className={`px-2 py-2 text-lg ${message_text.length > 50 && 'md:px-4'} lg:ml-10`}
         >
           {message_text}
         </p>
       </div>
       <div
-        className={`flex flex-1 flex-col items-end justify-between gap-2 ${isCurrentUser ? 'lg:mr-24' : ''}`}
+        className={`flex flex-1 flex-col items-end justify-between gap-2 ${isCurrentUser && 'lg:mr-24'}`}
       >
         <p className='text-sm font-light lg:text-base'>13:40</p>
-        <TickIcon read={is_read} />
+        {isCurrentUser && <TickIcon read={is_read} />}
       </div>
     </div>
   );
