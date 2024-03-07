3140746a2d59db768ab490d7c7c9b6287ea51619
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-03-07 09:39:12 +0000

    added describe block and updated test descriptor in login.spec.ts
    relates #86

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,13 +1,17 @@
 import { test } from '@playwright/experimental-ct-react';
 
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/login');
-  await page.getByPlaceholder('you@example.com').click();
-  await page.getByPlaceholder('you@example.com').fill('anderssji94@gmail.com');
-  await page.getByPlaceholder('••••••••').click();
-  await page
-    .getByPlaceholder('••••••••')
-    .fill('a password i dont use elsewhere');
-  await page.getByRole('button', { name: 'LOG IN' }).click();
-  await page.waitForURL('**/home-page');
+test.describe('/login', () => {
+  test('navigates user to /home-page', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.getByPlaceholder('you@example.com').click();
+    await page
+      .getByPlaceholder('you@example.com')
+      .fill('anderssji94@gmail.com');
+    await page.getByPlaceholder('••••••••').click();
+    await page
+      .getByPlaceholder('••••••••')
+      .fill('a password i dont use elsewhere');
+    await page.getByRole('button', { name: 'LOG IN' }).click();
+    await page.waitForURL('**/home-page');
+  });
 });
