8def38b2234a6fceca4d7c4fdafbee0f42631d66
Beth <bethazz@hotmail.co.uk>
2024-03-08 16:30:41 +0000

    remove testing files

--- tests/test-1.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/experimental-ct-react';
-
-test('test', async ({ page }) => {
-  await page.getByRole('link', { name: 'Log in' })
-  await page.getByPlaceholder('you@example.com')
-  await page.getByPlaceholder('you@example.com').fill('beth@foundersandcoders.com');
-  await page.getByPlaceholder('••••••••').click();
-});
