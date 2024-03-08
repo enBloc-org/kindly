443824d05c6542a1ada2a4c38a750767a580fec8
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-03-08 09:33:38 +0000

    renamed and rewrote profileButton.spec to profileRoute.spec
    relates #86

--- tests/profileButton.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from '@playwright/experimental-ct-react';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/profile');
-  await page.getByText('Log out').click();
-  await page.waitForURL('**/login')
-});
