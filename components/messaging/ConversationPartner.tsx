61d2c515283e18a9dc88e3bd98903c25e098289a
Beth <bethazz@hotmail.co.uk>
2024-03-13 17:15:39 +0000

    fix: update conversationPartner styling

--- components/messaging/ConversationPartner.tsx
+++ components/messaging/ConversationPartner.tsx
@@ -11,14 +11,14 @@ export const ConversationPartner: React.FC<ConversationCardProps> = ({
   conversation_partner,
 }) => {
   return (
-    <div className='flex flex-row p-5'>
+    <div className='flex flex-row items-center p-5'>
       <p data-testid='item-donor'>
         <b>From: </b>
         {conversation_partner.username}
       </p>
 
       <Image
-        className='ml-2'
+        className='ml-2 rounded-full'
         alt='user logo'
         width='25'
         height='35'
