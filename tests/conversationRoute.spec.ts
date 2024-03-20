69b4de4690d08df0f58e7c6a54955474da73849c
Beth <bethazz@hotmail.co.uk>
2024-03-20 11:15:34 +0000

    conversationRoute tests incomplete

--- tests/conversationRoute.spec.ts
+++ tests/conversationRoute.spec.ts
@@ -1,36 +1,45 @@
 import { test, expect } from '@playwright/experimental-ct-react';
 
 test.describe('/conversations', () => {
-  test('can be reached from /home-page', async ({ page }) => {
-    await page.goto('/home-page');
-    await page
-      .getByRole('link', { name: 'Image of Red Nike Trainers' })
-      .click();
-    await page.getByRole('button', { name: 'MESSAGE' }).click();
-    await expect(
-      page.locator('.conversation-height > div:nth-child(3)')
-    ).toBeVisible();
-  });
+  //     test('can be reached from /home-page', async ({ page }) => {
+  //       await page.goto('/home-page');
+  //       await page.getByTestId('div[data-testid="item-div"]').first().click(); // ?
+  //       await page.getByRole('button', { name: 'MESSAGE' }).click();
+  //       await expect(
+  //         page.locator('.conversation-height > div:nth-child(3)')
+  //       ).toBeVisible();
+  //     });
 
-  test('can send message', async ({ page }) => {
+  test('sends message', async ({ page }) => {
     await page.goto('/conversations');
     await page.getByPlaceholder('Type your message here').click();
     await page.getByPlaceholder('Type your message here').press('CapsLock');
-    await page.getByPlaceholder('Type your message here').fill('Nice shoes test');
+    await page
+      .getByPlaceholder('Type your message here')
+      .fill('Nice shoes test');
     await page.locator('form').getByRole('button').click();
     await expect(page.getByText('Nice shoes test')).toBeVisible();
   });
 
-  test('displays latest messages in CurrentConversation', async({page})=>{
+  test('displays latest messages in CurrentConversation', async ({ page }) => {
     await page.goto('/conversations');
     await page.getByPlaceholder('Type your message here').click();
     await page.getByPlaceholder('Type your message here').press('CapsLock');
-    await page.getByPlaceholder('Type your message here').fill('Displays latest message test');
+    await page
+      .getByPlaceholder('Type your message here')
+      .fill('Displays latest message test');
     await page.locator('form').getByRole('button').click();
-    await expect(page.locator('.conversation-height > .relative > div').first()).toBeVisible();
-  })
-    
-  // test('allows user to delete an existing conversation', async ({page})=>{
-  // ...
+    await expect(
+      page.locator('.conversation-height > .relative > div').first()
+    ).toBeVisible();
+  });
+
+  // test('deletes an existing conversation', async ({page})=>{
+  //   await page.goto('/conversations');
+  //   await page.locator('div:nth-child(16) > div').first().click();
+  //   await page.locator('div:nth-child(16) > div > div > .ml-auto > .rounded-md').click();
+  //   await page.getByRole('button', { name: 'Delete Conversation' }).click();
+  //   await page.getByLabel('Delete').click();
+  //   await expect(page.locator('div:nth-child(16) > div').first()).not.toBeVisible()!;
   // })
 });
