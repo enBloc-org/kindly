43fb02fe9ddba7a17733ed9d4d18fd6a0d3ef2ee
Beth <bethazz@hotmail.co.uk>
2024-03-20 09:36:12 +0000

    update route to remove url

--- tests/CurrentConversation.spec.ts
+++ tests/CurrentConversation.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('CurrentConversation', () => {
   test('displays item donor username', async ({ page }) => {
-    await page.goto('http://localhost:3000/conversations');
+    await page.goto('/conversations');
     await page.getByTestId('item-donor').click();
     expect(await page.waitForSelector('p[data-testid="item-donor"]'));
   });
--- tests/conversationRoute.spec.ts
+++ tests/conversationRoute.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/experimental-ct-react';
 
 test.describe('/conversations', () => {
   test('can be reached from /home-page', async ({ page }) => {
-    await page.goto('http://localhost:3000/home-page');
+    await page.goto('/home-page');
     await page
       .getByRole('link', { name: 'Image of Red Nike Trainers' })
       .click();
@@ -13,18 +13,23 @@ test.describe('/conversations', () => {
   });
 
   test('can send message', async ({ page }) => {
-    await page.goto('http://localhost:3000/conversations');
+    await page.goto('/conversations');
     await page.getByPlaceholder('Type your message here').click();
     await page.getByPlaceholder('Type your message here').press('CapsLock');
-    await page.getByPlaceholder('Type your message here').fill('Nice shoes');
+    await page.getByPlaceholder('Type your message here').fill('Nice shoes test');
     await page.locator('form').getByRole('button').click();
-    await expect(page.getByText('Nice shoes')).toBeVisible();
+    await expect(page.getByText('Nice shoes test')).toBeVisible();
   });
 
-  // test('displays latest messages in CurrentConversation', async({page})=>{
-  // ...
-  // })
-
+  test('displays latest messages in CurrentConversation', async({page})=>{
+    await page.goto('/conversations');
+    await page.getByPlaceholder('Type your message here').click();
+    await page.getByPlaceholder('Type your message here').press('CapsLock');
+    await page.getByPlaceholder('Type your message here').fill('Displays latest message test');
+    await page.locator('form').getByRole('button').click();
+    await expect(page.locator('.conversation-height > .relative > div').first()).toBeVisible();
+  })
+    
   // test('allows user to delete an existing conversation', async ({page})=>{
   // ...
   // })
