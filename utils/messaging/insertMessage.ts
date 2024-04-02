38e7d60004fd22764dafc0215131214fde3f0596
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2024-04-02 15:58:08 +0100

    feat: updated insert message function to update the user conversation table to show unread messages and handle errors
    Relates #72

--- utils/messaging/insertMessage.ts
+++ utils/messaging/insertMessage.ts
@@ -5,13 +5,33 @@ export default async function insertMessage(
   conversation_id: number | undefined,
   message: string
 ) {
-  const supabase = newClient();
-  await supabase.from('messages').insert([
-    {
-      conversation_id: conversation_id,
-      sender_id: user_id,
-      message_text: message,
-      is_read: false,
-    },
-  ]);
+  try {
+    const supabase = newClient();
+    const { error: messageError } = await supabase.from('messages').insert([
+      {
+        conversation_id: conversation_id,
+        sender_id: user_id,
+        message_text: message,
+        is_read: false,
+      },
+    ]);
+
+    if (messageError)
+      throw new Error(`Error inserting message: ${messageError.message}`);
+    const { error: updateError } = await supabase
+      .from('user_conversations')
+      .update({ has_unread_messages: true })
+      .match({ conversation_id: conversation_id }) // Match the conversation ID
+      .not('user_id', 'eq', user_id); // Exclude the sender from the update
+
+    if (updateError)
+      throw new Error(
+        `Error updating user_conversations: ${updateError.message}`
+      );
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(error.message);
+      throw error;
+    }
+  }
 }
