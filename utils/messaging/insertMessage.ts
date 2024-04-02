622604bfc28d2a81822690f32bb3b9e96b1adee2
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2024-04-02 16:16:51 +0100

    style: update argument naming
    Relates #72

--- utils/messaging/insertMessage.ts
+++ utils/messaging/insertMessage.ts
@@ -1,16 +1,16 @@
 import newClient from '../../config/supabaseclient';
 
 export default async function insertMessage(
-  user_id: string | undefined,
-  conversation_id: number | undefined,
+  userId: string | undefined,
+  conversationId: number | undefined,
   message: string
 ) {
   try {
     const supabase = newClient();
     const { error: messageError } = await supabase.from('messages').insert([
       {
-        conversation_id: conversation_id,
-        sender_id: user_id,
+        conversation_id: conversationId,
+        sender_id: userId,
         message_text: message,
         is_read: false,
       },
@@ -18,12 +18,12 @@ export default async function insertMessage(
 
     if (messageError)
       throw new Error(`Error inserting message: ${messageError.message}`);
+    
     const { error: updateError } = await supabase
       .from('user_conversations')
       .update({ has_unread_messages: true })
-      .match({ conversation_id: conversation_id }) // Match the conversation ID
-      .not('user_id', 'eq', user_id); // Exclude the sender from the update
-
+      .match({ conversation_id: conversationId })
+      .not('user_id', 'eq', userId);
     if (updateError)
       throw new Error(
         `Error updating user_conversations: ${updateError.message}`
