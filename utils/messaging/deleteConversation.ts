9a78732708972ae4c04c6281a497043a334e9687
Beth <bethazz@hotmail.co.uk>
2024-04-11 11:28:20 +0100

    add bool update util function

--- utils/messaging/deleteConversation.ts
+++ utils/messaging/deleteConversation.ts
@@ -1,4 +1,5 @@
 import newClient from '../../config/supabaseclient';
+import { markAsDeleted } from './markAsDeleted';
 
 export default async function deleteConversation(
   conversationId: number,
@@ -11,7 +12,10 @@ export default async function deleteConversation(
       .delete()
       .match({ conversation_id: conversationId, user_id: userId });
 
+      markAsDeleted(conversationId, true)
+
     if (error) throw error;
+
   } catch (error) {
     console.error('Error deleting conversation:', error);
   }
