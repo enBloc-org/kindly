3da1296f573e2be340829bfc298296255e4a558d
Yurii Petrovskyi <yurakarpaty@gmail.com>
2024-05-06 15:10:11 +0100

    feat: added additional validation to the messaging function newConvoStart.ts
    relates #141

--- supabase/models/messaging/newConvoStart.ts
+++ supabase/models/messaging/newConvoStart.ts
@@ -8,13 +8,40 @@ export default async function newConvoStart(
   if (userID && donorID && itemID) {
     try {
       const supabase = newClient();
-      const { data: conversationData } = await supabase
+      /**
+       * @description before creating a new conversation, we check if such a conversation already exists in the database,
+       * if we find such a conversation, we return its ID that we can use further in the code,
+       * if such a conversation does not exist in the database, then we create a new conversation
+       */
+
+      // Checking if there is already a conversation between these users about this product
+      const { data: existingConversations, error } = await supabase
+        .from('user_conversations')
+        .select('conversation_id')
+        .eq('user_id', userID)
+        .eq('partner_id', donorID)
+        .eq('item_id', itemID);
+      if (error) {
+        console.error('Error checking for existing conversations:', error);
+        return null;
+      }
+      // If the conversation already exists, we return its ID
+      if (existingConversations.length > 0) {
+        console.log('such a conversation already exists');
+        return existingConversations[0].conversation_id;
+      }
+      // Creating a new conversation
+      const { data: newConversation, error: insertError } = await supabase
         .from('conversations')
         .insert([{}])
         .select('id')
         .single();
-      const conID = conversationData?.id;
-
+      if (insertError) {
+        console.error('Error inserting new conversation:', insertError);
+        return null;
+      }
+      // Registration of new conversation participants
+      const conID = newConversation?.id;
       await supabase.from('user_conversations').insert([
         {
           conversation_id: conID,
@@ -29,12 +56,15 @@ export default async function newConvoStart(
           partner_id: userID,
         },
       ]);
+      return null;
     } catch (error) {
       console.error(error);
+      return null;
     }
   } else {
     alert(
       'Something went wrong! Please retry messaging - you may need to log back in.'
     );
+    return null;
   }
 }
