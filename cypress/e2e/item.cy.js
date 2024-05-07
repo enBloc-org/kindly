8e3865359419c68fa6b41676b8721e809ffa3c26
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2024-05-07 17:06:31 +0100

    refactor: added grow to message container
    Relates #73

---
+++ cypress/e2e/item.cy.js
@@ -0,0 +1,38 @@
+import AddItemPage from '../support/page_objects/AddItemPage';
+import ProfilePage from '../support/page_objects/profilePage';
+import * as page from '../fixtures/URLs.json';
+import * as data from '../fixtures/inputData.json';
+
+describe('Create and Delete item positive test Suite', () => {
+  const uniqueItemName = `${data.itemName}_${Date.now()}`;
+  beforeEach(() => {
+    cy.login(Cypress.env('loginEmail'), Cypress.env('loginPassword'));
+  });
+
+  it('User can create an item. The item is visible in user profile', () => {
+    cy.visit(page.addItem);
+    AddItemPage.nameInput().type(uniqueItemName);
+    AddItemPage.descriptionInput().type(data.itemDescription);
+    AddItemPage.postCodeInput().type(data.postCode);
+    AddItemPage.conditionDropDown().select(data.itemCondition);
+    AddItemPage.categoriesDropDown().select(data.itemCategory);
+    AddItemPage.sizeInput().type(data.itemShoesSize);
+    AddItemPage.genderDropDown().select(data.itemGender);
+    AddItemPage.willingToPostCheckBox().check();
+    AddItemPage.postageCoveredCheckBox().check();
+    AddItemPage.uploadImageButton().selectFile(
+      'cypress/fixtures/images/converse.jpg'
+    );
+    AddItemPage.addYourItemButton().click();
+    AddItemPage.itemAddedMessage().should('be.visible');
+    AddItemPage.profileIcon().click();
+    ProfilePage.itemCard(uniqueItemName).should('be.visible');
+  });
+
+  it('User can delete the item', () => {
+    cy.visit(page.profile);
+    ProfilePage.deleteItemButton(uniqueItemName).click();
+    ProfilePage.ConfirmDeleteItemButton(uniqueItemName).click();
+    ProfilePage.itemCard(uniqueItemName).should('not.exist');
+  });
+});
