54645c04a773033c3c8976569dd03ecd04a5776c
kkageg <gelya.gelentwagen@gmail.com>
2024-05-07 01:25:29 +0100

    test: added two tests for create and delete item functionality; made the tests more stable by generating uniqueItemName with current timestamp; updated viewport resolution for testing to FullHD (1920x1080).

--- cypress/support/page_objects/basePage.js
+++ cypress/support/page_objects/basePage.js
@@ -2,6 +2,10 @@ export class BasePage {
   homeButton() {
     return cy.get('a[href="/home-page"]').should('be.visible');
   }
+
+  profileIcon() {
+    return cy.get('a[href="/profile"]');
+  }
 }
 
 export default BasePage;
