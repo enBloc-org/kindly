cbc69ac38cbef629abbc71ac422bcddc837ab208
ShaughnAnderson94 <anderssji94@gmail.com>
2024-05-13 11:53:09 +0100

    fixed for beth

--- components/messaging/MessageForm.tsx
+++ components/messaging/MessageForm.tsx
@@ -1,10 +1,12 @@
 'use client';
 import { FormEvent, useState, useRef } from 'react';
 import convoRestart from '@/utils/messaging/convoRestart';
+import { useConversationContext } from '../../context/conversationContext';
 
 // Components
 import insertMessage from '@/utils/messaging/insertMessage';
 import PaperPlaneIcon from '../icons/PaperPlaneIcon';
+import { ConversationCardType } from '@/types/messagingTypes';
 
 type MessageFormProps = {
   user_id: string | undefined;
@@ -23,7 +25,8 @@ const MessageForm: React.FC<MessageFormProps> = ({
 }) => {
   const [message, setMessage] = useState<string>('');
   const textareaRef = useRef<HTMLTextAreaElement>(null);
-
+  const { currentConversation, setCurrentConversation } =
+    useConversationContext();
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
 
@@ -40,7 +43,23 @@ const MessageForm: React.FC<MessageFormProps> = ({
 
     if (member_has_deleted) {
       try {
+        console.log('code knows deleted is true');
         await convoRestart(conversation_id, user_id, partner_id, item_id);
+        console.log('initial current conversation');
+        console.log(currentConversation);
+        // console.log("what should be going in to set Current conversation")
+        // console.log({...currentConversation, ...conversations,  member_has_deleted: false  })
+
+        currentConversation &&
+          setCurrentConversation({
+            ...currentConversation,
+            conversations: {
+              ...currentConversation.conversations, // Keep other properties
+              member_has_deleted: false, // Update only this property
+            },
+          } as ConversationCardType);
+        console.log('final current conversation');
+        console.log(currentConversation);
       } catch (error) {
         console.error('error');
       }
