91d7bfef43d9b8d23aa86c94ba5e1125ded901b2
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2024-05-13 23:29:29 +0100

    refactor: added id to all conversation type and select call to fix deletion
    Relates #181

--- supabase/models/messaging/selectUserConversations.ts
+++ supabase/models/messaging/selectUserConversations.ts
@@ -1,20 +1,23 @@
 import newClient from '@/supabase/utils/newClient';
 import { AllConversationsType } from '../../../types/messagingTypes';
+import { PostgrestError } from '@supabase/supabase-js';
 
 const selectUserConversations = async (
   userId?: string
 ): Promise<AllConversationsType> => {
   try {
     const supabase = newClient();
-    const { data, error } = await supabase.rpc('fetch_user_conversations', {
-      p_user_id: userId,
-    });
+    const {
+      data,
+      error,
+    }: { data: AllConversationsType | null; error: PostgrestError | null } =
+      await supabase.rpc('fetch_user_conversations', {
+        p_user_id: userId,
+      });
 
     if (error) throw error;
 
-    const conversations = data as unknown as AllConversationsType;
-
-    return conversations ?? [];
+    return data ?? [];
   } catch (error) {
     console.error(`Failed to fetch conversations from database: ${error}`);
     throw error;
