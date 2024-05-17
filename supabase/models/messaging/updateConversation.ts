6eea862795d4c60705a845cdc5b2711f7e1c4c70
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-05-17 22:15:35 +0100

    feat: new util to update existing conversations in user_conversations
    relates #69

---
+++ supabase/models/messaging/updateConversation.ts
@@ -0,0 +1,19 @@
+import newClient from '@/supabase/utils/newClient';
+
+export default async function updateConversation(
+  conversationId: number,
+  userId: string
+) {
+  try {
+    const supabase = newClient();
+    const { error } = await supabase
+      .from('user_conversations')
+      .upsert({ has_unread_messages: true })
+      .match({ conversation_id: conversationId, user_id: userId });
+
+    if (error) throw error;
+  } catch (error) {
+    console.error(`Error marking conversation as unread: ${error}`);
+    throw error;
+  }
+}
