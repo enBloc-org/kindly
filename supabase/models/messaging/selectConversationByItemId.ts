78b21f0a6df58ca49ab596ad3b2e0dc262480b37
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-05-24 12:49:37 +0100

    feat: new util to retrieve all conversation ids associated with an item
    relates #169

---
+++ supabase/models/messaging/selectConversationByItemId.ts
@@ -0,0 +1,21 @@
+import newClient from '@/supabase/utils/newClient';
+
+export default async function selectConversationByItemId(itemId: number) {
+  try {
+    const supabase = newClient();
+    const { data, error } = await supabase
+      .from('user_conversations')
+      .select('conversation_id')
+      .eq('item_id', itemId);
+
+    if (error) throw error;
+
+    const conversationsSet = new Set(
+      data.map((conversation) => conversation.conversation_id)
+    );
+    return conversationsSet;
+  } catch (error) {
+    console.error(`Error retrieving conversations: ${error}`);
+    throw error;
+  }
+}
