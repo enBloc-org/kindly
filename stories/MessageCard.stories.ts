325230e22dc58936ec56de259074ef24d95e264f
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-05-24 23:22:20 +0100

    feat: basic MessageCard stories
    relates #221

---
+++ stories/MessageCard.stories.ts
@@ -0,0 +1,52 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import MessageCard from '@/components/messaging/MessageCard';
+
+const meta = {
+  title: 'Messaging/MessageCard',
+  component: MessageCard,
+} satisfies Meta<typeof MessageCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const ShortMessageFromSelf: Story = {
+  args: {
+    senderId: 'testuser',
+    createdAt: '24/05/2024',
+    messageText: 'Short test message',
+    currentUser: 'testuser',
+    messageId: 3,
+  },
+};
+
+export const LongMessageFromSelf: Story = {
+  args: {
+    senderId: 'testuser',
+    createdAt: '24/05/2024',
+    messageText:
+      'This is a really long message and it even includes \nodd \n\n    formatting!! 🤪',
+    currentUser: 'testuser',
+    messageId: 3,
+  },
+};
+
+export const ShortMessageFromOther: Story = {
+  args: {
+    senderId: 'testuser',
+    createdAt: '24/05/2024',
+    messageText: 'Short test message',
+    currentUser: 'anotheruser',
+    messageId: 3,
+  },
+};
+
+export const LongMessageFromOther: Story = {
+  args: {
+    senderId: 'testuser',
+    createdAt: '24/05/2024',
+    messageText:
+      "This message is really long! \nIn fact, it is much too long!! \n\nIt's very uncommon indeed 🤔",
+    currentUser: 'anotheruser',
+    messageId: 3,
+  },
+};
