004fc7d265cf78868c6528381c3ce6794ee6569e
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-05-24 23:21:15 +0100

    feat: added preview background settings
    relates #221

---
+++ .storybook/preview.ts
@@ -0,0 +1,30 @@
+import type { Preview } from '@storybook/react';
+import 'app/globals.css';
+
+const preview: Preview = {
+  parameters: {
+    backgrounds: {
+      default: 'kindly',
+      values: [
+        {
+          name: 'kindly',
+          value: '#F0EFED',
+        },
+        {
+          name: 'currentConversation',
+          value: '#FAFAF9',
+        },
+      ],
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+
+  tags: ['autodocs'],
+};
+
+export default preview;
