ae6e79b3e4b38de6205b347bef56f7d2d0cf1656
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-05-28 17:40:28 +0100

    test: ConversationCard Story interaction for ellipsis menu options
    relates #221

--- stories/messaging/ConversationCard.stories.tsx
+++ stories/messaging/ConversationCard.stories.tsx
@@ -3,6 +3,7 @@ import { userEvent, within, expect, waitFor, fn } from '@storybook/test';
 
 import ConversationCard from '@/components/messaging/ConversationCard';
 import ConversationContextProvider from '@/context/conversationContext';
+import profilePicture from 'public/clothing-donation.jpg';
 
 const meta: Meta<typeof ConversationCard> = {
   title: 'Messaging/ConversationCard',
@@ -22,22 +23,60 @@ const meta: Meta<typeof ConversationCard> = {
       await userEvent.click(canvas.getByTestId('card-wrapper'));
     });
     await waitFor(() => expect(args.clickHandler).toHaveBeenCalled());
+
+    await step('Open Ellipsis Menu', async () => {
+      await userEvent.click(canvas.getByRole('button'));
+    });
+    await waitFor(() =>
+      expect(canvas.queryByText('Delete Conversation')).toBeVisible()
+    );
+    await waitFor(() =>
+      expect(canvas.queryByText('Mark Unread')).toBeVisible()
+    );
   },
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const ConversationCardDefault: Story = {
+export const ConversationCardWithUnreadMessage: Story = {
   args: {
     conversationId: 2,
-    messageTimestamp: new Date().toString(),
+    messageTimestamp: '2024-01-25 13:00:56+00',
+    messageText:
+      'This is the last message in the conversation, and it might be really long.',
+    partnerUsername: 'Jane Doe',
+    partnerAvatar: profilePicture.src,
+    itemName: 'White Jumper',
+    clickHandler: fn(),
+    notificationList: [1, 2, 3],
+  },
+};
+
+export const ConversationCardWithoutUnreadMessage: Story = {
+  args: {
+    conversationId: 2,
+    messageTimestamp: '2024-01-25 13:00:56+00',
+    messageText:
+      'This is the last message in the conversation, and it might be really long.',
+    partnerUsername: 'Jane Doe',
+    partnerAvatar: profilePicture.src,
+    itemName: 'White Jumper',
+    clickHandler: fn(),
+    notificationList: [1, 3],
+  },
+};
+
+export const ConversationCardWithDefaultUserAvatar: Story = {
+  args: {
+    conversationId: 2,
+    messageTimestamp: '2024-01-25 13:00:56+00',
     messageText:
       'This is the last message in the conversation, and it might be really long.',
     partnerUsername: 'Jane Doe',
     partnerAvatar: undefined,
     itemName: 'White Jumper',
     clickHandler: fn(),
-    notificationList: [1, 2, 3],
+    notificationList: [1, 3],
   },
 };
