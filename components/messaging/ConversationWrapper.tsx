61f54f1a258f72062b8e9e92b5f0a7f6bd336dce
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2024-06-05 12:28:48 +0100

    refactor: ConversationWrapper update to useReducer
    Relates #236

--- components/messaging/ConversationWrapper.tsx
+++ components/messaging/ConversationWrapper.tsx
@@ -4,25 +4,25 @@ import { useEffect } from 'react';
 import ConversationsList from './ConversationsList';
 import CurrentConversation from './CurrentConversation';
 import useMediaQuery from '../hooks/useMediaQuery';
-import getUserConversationsandItemNames from '@/supabase/models/messaging/selectUserConversations';
+import selectUserConversations from '@/supabase/models/messaging/selectUserConversations';
 import { useConversationContext } from '@/context/conversationContext';
 
-type ConversationWrapperType = {
-  userId: string;
-};
-
-const ConversationWrapper: React.FC<ConversationWrapperType> = ({ userId }) => {
+const ConversationWrapper: React.FC<string> = (userId) => {
   const isBreakpoint = useMediaQuery(1000);
-  const { showConversationsList, setAllConversations, setCurrentUserId } =
-    useConversationContext();
+  const {
+    conversationState: { showConversationsList },
+    dispatch,
+  } = useConversationContext();
 
   useEffect(() => {
     const fetchConversations = async () => {
-      const fetchedConversations =
-        await getUserConversationsandItemNames(userId);
+      const fetchedConversations = await selectUserConversations(userId);
 
-      setAllConversations(fetchedConversations);
-      setCurrentUserId(userId);
+      dispatch({
+        type: 'SET_ALL_CONVERSATIONS',
+        payload: fetchedConversations,
+      });
+      dispatch({ type: 'SET_CURRENT_USER_ID', payload: userId });
     };
     fetchConversations();
   }, []);
