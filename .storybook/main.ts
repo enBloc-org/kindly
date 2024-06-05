7f724aa6919cd7e37036be1d471b48e753e32c94
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-06-05 16:39:55 +0100

    build: corrected path for .mdx files in the codebase
    relates #221

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../**/*.mdx', '../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
