3d0051248db27d19ed1cc6ddef4df6262cd876da
Alexander <alex@alexvoiceover.com>
2024-06-10 20:52:42 +0100

    fix: Deleted file pageType.ts
    Relates #190

--- app/(dashboard)/add-item/pageTypes.ts
+++
@@ -1,6 +0,0 @@
-import { PartialItem } from '@/types/supabaseTypes';
-
-//This type extends PartialItem with the boolean imaheUploaded
-export type ExtendedPartialItem = PartialItem & {
-  imageUploaded?: boolean;
-};
