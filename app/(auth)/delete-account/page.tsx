37a23e893347db35b4d07f3cb2f8b9c4579c5971
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-06-28 10:46:40 +0100

    feat: added submission handling for AccountDeleteForm
    relates #258

--- app/(auth)/delete-account/page.tsx
+++ app/(auth)/delete-account/page.tsx
@@ -4,18 +4,20 @@ import { useEffect, useState } from 'react';
 import AccountDeleteForm from '@/components/AccountDeleteForm';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { getProfile } from '@/supabase/models/getProfile';
+import newClient from '@/supabase/utils/newClient';
 
 export default function DeleteAccount() {
   const [loggedUser, setLoggedUser] = useState('');
+  const [userId, setUserId] = useState<undefined | string>(undefined);
 
   useEffect(() => {
     const getUserId = async () => {
       const supabase = createClientComponentClient();
       const { data: userData } = await supabase.auth.getSession();
-      const user = userData.session?.user.id;
+      setUserId(userData.session?.user.id);
 
-      if (user) {
-        const userName = await getProfile(user);
+      if (userId) {
+        const userName = await getProfile(userId);
         setLoggedUser(userName.data.username);
       }
     };
@@ -23,5 +25,27 @@ export default function DeleteAccount() {
     getUserId();
   }, []);
 
-  return <AccountDeleteForm currentUserName={loggedUser} />;
+  const accountDeleteHandler = () => {
+    const supabase = newClient();
+    const deleteUser = async () => {
+      if (userId) {
+        try {
+          const { data } = await supabase.auth.admin.deleteUser(userId);
+          console.log(data);
+        } catch (error) {
+          console.error(`Error deleting account: ${error}`);
+          throw error;
+        }
+      }
+    };
+
+    deleteUser();
+  };
+
+  return (
+    <AccountDeleteForm
+      currentUserName={loggedUser}
+      submitHandler={accountDeleteHandler}
+    />
+  );
 }
--- components/AccountDeleteForm.tsx
+++ components/AccountDeleteForm.tsx
@@ -1,17 +1,29 @@
 'use client';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 export default function AccountDeleteForm({
   currentUserName,
+  submitHandler,
 }: {
   currentUserName: string;
+  submitHandler: () => void;
 }) {
   const [userName, setUserName] = useState('');
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    if (userName === currentUserName) {
+      submitHandler();
+    } else {
+      alert('The username you submitted is incorrect');
+    }
+  };
 
   return (
     <div>
       <h1>We are sorry to see you go 😢</h1>
-      <form>
+      <form onSubmit={(event) => handleSubmit(event)}>
         Please enter your userName to confirm deleting your DeleteAccount
         <input
           value={userName}
@@ -19,6 +31,7 @@ export default function AccountDeleteForm({
           onChange={(event) => setUserName(event.target.value)}
           placeholder={currentUserName}
         />
+        <button>Delete my account forever</button>
       </form>
     </div>
   );
