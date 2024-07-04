36404176cdf11f74e56e481f2be4496bc7a7270c
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-07-04 15:14:27 +0100

    refactor: using bespokequery to delete user matching the logged session
    relates #258

--- supabase/models/deleteProfile.ts
+++ supabase/models/deleteProfile.ts
@@ -5,10 +5,13 @@ import newClient from '../utils/newClient';
 export default async function deleteProfile(userId: string) {
   const supabase = newClient();
   try {
-    const { data } = await supabase.auth.admin.deleteUser(userId);
+    const { data } = await supabase
+      .from('profiles')
+      .delete()
+      .eq('id', userId)
+      .select();
     console.log(data);
   } catch (error) {
-    console.error(`Error deleting account: ${error}`);
     throw error;
   }
 }
