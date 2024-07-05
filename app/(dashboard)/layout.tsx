5ba2d05ff350858a5f4e9756b6437c85516b841c
Miguel Nixo <mnixo@users.noreply.github.com>
2024-07-05 14:05:05 +0100

    feat: avoid redirecting to login from home

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,4 @@
 import Header from '@/components/Header';
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { redirect } from 'next/navigation';
-import { cookies } from 'next/headers';
 import Footer from '@/components/Footer';
 import Providers from '@/context/Providers';
 import FullHeightContainer from '@/components/layout/FullHeightComponent';
@@ -11,11 +8,6 @@ export default async function DashBoardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const supabase = createServerComponentClient({ cookies });
-  const { data } = await supabase.auth.getSession();
-  if (!data.session) {
-    redirect('/login');
-  }
   return (
     <>
       <Providers>
