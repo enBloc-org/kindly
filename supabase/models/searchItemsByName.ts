c84cbfdba85974c965c5e1099fca1f6826987542
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2024-07-09 17:11:21 +0100

    feat: search category also searches subtype from items table
    Relates: #256

--- supabase/models/searchItemsByName.ts
+++ supabase/models/searchItemsByName.ts
@@ -2,7 +2,7 @@ import { item } from '../../types/supabaseTypes';
 import newClient from '../utils/newClient';
 
 export default async function searchItemsByName(
-  name: string,
+  searchTerm: string,
   limit: number,
   cursor: string
 ): Promise<item[]> {
@@ -12,7 +12,9 @@ export default async function searchItemsByName(
     let query = supabase
       .from('items')
       .select()
-      .or(`item_name.ilike.%${name}%,item_type.ilike.%${name}%`)
+      .or(
+        `item_name.ilike.%${searchTerm}%,item_type.ilike.%${searchTerm}%,item_subtype.ilike.%${searchTerm}%`
+      )
       .order('created_at', { ascending: false })
       .limit(Number(limit));
 
