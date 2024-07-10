3a4e63ed0d8f8436fe97c772d2d2ecb0a47848a4
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-07-10 17:51:56 +0100

    refactor: wrapped display in a container component to isolate database logic in the server according to our convention
    relates #258

--- app/(auth)/delete-account/page.tsx
+++ app/(auth)/delete-account/page.tsx
@@ -1,72 +1,28 @@
-'use client';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-
-import AccountDeleteForm from '@/components/AccountDeleteForm';
-import Modal from '@/components/Modal';
+'use server';
+import AccountDeleteFormContainer from '@/components/AccountDeleteFormContainer';
 import { getProfile } from '@/supabase/models/getProfile';
-import deleteProfile from '@/supabase/models/deleteProfile';
-import newClient from '@/supabase/utils/newClient';
+import newServerClient from '@/supabase/utils/newServerClient';
 
-export default function DeleteAccount() {
-  const [loggedUser, setLoggedUser] = useState('');
-  const [userId, setUserId] = useState<undefined | string>(undefined);
-  const [isConfirmed, setIsConfirmed] = useState<boolean>(false);
-  const router = useRouter();
+export default async function DeleteAccount() {
+  const supabase = newServerClient();
+  try {
+    const {
+      data: { user },
+      error,
+    } = await supabase.auth.getUser();
+    if (error) throw error;
+    if (user) {
+      const {
+        data: { username },
+        error,
+      } = await getProfile(user.id);
+      if (error) throw error;
 
-  useEffect(() => {
-    const getUserId = async () => {
-      const supabase = newClient();
-      try {
-        const { data: userData } = await supabase.auth.getSession();
-        setUserId(userData.session?.user.id);
-      } catch (error) {
-        console.error(`Error getting session: ${error}`);
-      }
-
-      if (userId) {
-        const userName = await getProfile(userId as string);
-        setLoggedUser(userName.data.username);
-      }
-    };
-
-    getUserId();
-  }, [setLoggedUser, setUserId, userId]);
-
-  const formSubmitHandler = () => {
-    setIsConfirmed(true);
-  };
-
-  const accountDeleteHandler = () => {
-    const deleteUser = async () => {
-      if (userId !== undefined) {
-        try {
-          await deleteProfile(userId);
-          router.push('/');
-        } catch (error) {
-          console.error(error);
-          throw error;
-        }
-      }
-    };
-
-    deleteUser();
-  };
-
-  return (
-    <div className='flex flex-col  items-center  px-8'>
-      <AccountDeleteForm
-        currentUserName={loggedUser}
-        submitHandler={formSubmitHandler}
-      />
-
-      <Modal
-        name='Delete Profile'
-        message='Confirming will delete your profile permanently. Are you sure you want to continue?'
-        targetId={userId as string}
-        onDeleteSuccess={accountDeleteHandler}
-        isDisabled={!isConfirmed}
-      />
-    </div>
-  );
+      return (
+        <AccountDeleteFormContainer userName={username} userId={user.id} />
+      );
+    }
+  } catch (error) {
+    throw error;
+  }
 }
