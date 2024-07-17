ebbbc92d6e70aa1cd91905f2c7e52b3859c66039
Yurii Petrovskyi <yurakarpaty@gmail.com>
2024-07-17 17:22:22 +0100

    feat: remove if else block and refactor the func starNewtConversation to return a conversation object or throw an error
    relates #244

--- supabase/models/messaging/startNewConversation.ts
+++ supabase/models/messaging/startNewConversation.ts
@@ -6,91 +6,85 @@ import insertSystemMessage from './insertSystemMessage';
  * @param userID Id of the user currently logged in
  * @param donorID Id of the user who is donating this item
  * @param itemID Id of the item which the new conversation will relate to
- * @returns null if no conversation existed previously or a conversation object if an existing conversation has been found
+ * @returns A conversation object of an existing conversation or a newly created conversation. Throws an error if any error occurs.
  */
 export default async function startNewConversation(
   userID: string | undefined,
   donorID: string,
   itemID: string
 ) {
-  if (userID && donorID && itemID) {
-    try {
-      const supabase = newClient();
+  try {
+    const supabase = newClient();
 
-      const { data: existingConversations, error: userConversationsError } =
-        await supabase
-          .from('user_conversations')
-          .select('*')
-          .eq('user_id', userID)
-          .eq('partner_id', donorID)
-          .eq('item_id', itemID);
-      if (userConversationsError) {
-        console.error(
-          'Error checking for existing conversations:',
-          userConversationsError
-        );
-        return null;
-      }
-      if (existingConversations.length > 0) {
-        return existingConversations[0];
-      }
-
-      const { data: newConversation, error: insertError } = await supabase
-        .from('conversations')
-        .insert([{}])
+    const { data: existingConversations, error: userConversationsError } =
+      await supabase
+        .from('user_conversations')
         .select('*')
-        .single();
-      if (insertError) {
-        console.error('Error inserting new conversation:', insertError);
-        return null;
-      }
-      const conversationId = newConversation?.id;
-
-      await supabase.from('user_conversations').insert([
-        {
-          conversation_id: conversationId,
-          user_id: userID,
-          item_id: itemID,
-          partner_id: donorID,
-        },
-        {
-          conversation_id: conversationId,
-          user_id: donorID,
-          item_id: itemID,
-          partner_id: userID,
-        },
-      ]);
-
-      await insertSystemMessage(
-        conversationId,
-        'This is the start of your conversation.'
+        .eq('user_id', userID)
+        .eq('partner_id', donorID)
+        .eq('item_id', itemID);
+    if (userConversationsError) {
+      throw new Error(
+        'Error checking for existing conversations:' +
+          userConversationsError.message
       );
-
-      const { data: newUserConversation, error: newUserConversationError } =
-        await supabase
-          .from('user_conversations')
-          .select('*')
-          .eq('user_id', userID)
-          .eq('partner_id', donorID)
-          .eq('item_id', itemID)
-          .single();
-
-      if (newUserConversationError) {
-        console.error(
-          'Error fetching new user conversation:',
-          newUserConversationError
-        );
-        return null;
-      }
-      return newUserConversation;
-    } catch (error) {
-      console.error(error);
-      return null;
     }
-  } else {
-    alert(
-      'Something went wrong! Please retry messaging - you may need to log back in.'
+    if (existingConversations.length > 0) {
+      return existingConversations[0];
+    }
+
+    const { data: newConversation, error: insertError } = await supabase
+      .from('conversations')
+      .insert([{}])
+      .select('*')
+      .single();
+    if (insertError) {
+      throw new Error(
+        'Error inserting new conversation: ' + insertError.message
+      );
+    }
+    const conversationId = newConversation?.id;
+
+    await supabase.from('user_conversations').insert([
+      {
+        conversation_id: conversationId,
+        user_id: userID,
+        item_id: itemID,
+        partner_id: donorID,
+      },
+      {
+        conversation_id: conversationId,
+        user_id: donorID,
+        item_id: itemID,
+        partner_id: userID,
+      },
+    ]);
+
+    await insertSystemMessage(
+      conversationId,
+      'This is the start of your conversation.'
     );
-    return null;
+
+    const { data: newUserConversation, error: newUserConversationError } =
+      await supabase
+        .from('user_conversations')
+        .select('*')
+        .eq('user_id', userID)
+        .eq('partner_id', donorID)
+        .eq('item_id', itemID)
+        .single();
+
+    if (newUserConversationError) {
+      throw new Error(
+        'Error fetching new user conversation: ' +
+          newUserConversationError.message
+      );
+    }
+
+    return newUserConversation;
+  } catch (error) {
+    throw new Error(
+      'Failed to start a new conversation: ' + (error as Error).message
+    );
   }
 }
