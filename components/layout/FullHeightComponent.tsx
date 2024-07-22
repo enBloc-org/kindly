9d4cee805afba574df17cf210671afc10978e389
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-07-22 16:36:01 +0100

    fix: removed evaluation that was causing hydration error
    relates #285

--- components/layout/FullHeightComponent.tsx
+++ components/layout/FullHeightComponent.tsx
@@ -3,28 +3,26 @@
 import { ReactNode, useEffect, useState } from 'react';
 
 const FullHeightContainer = ({ children }: { children: ReactNode }) => {
-  if (typeof window !== 'undefined') {
-    const [height, setHeight] = useState(window.innerHeight);
+  const [height, setHeight] = useState(window.innerHeight);
 
-    useEffect(() => {
-      const handleResize = () => {
-        setHeight(window.innerHeight);
-      };
-
-      window.addEventListener('resize', handleResize);
-
-      return () => {
-        window.removeEventListener('resize', handleResize);
-      };
-    }, []);
-
-    const style = {
-      height: height + 'px',
-      overflow: 'auto',
+  useEffect(() => {
+    const handleResize = () => {
+      setHeight(window.innerHeight);
     };
 
-    return <div style={style}>{children}</div>;
-  }
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const style = {
+    height: height + 'px',
+    overflow: 'auto',
+  };
+
+  return <div style={style}>{children}</div>;
 };
 
 export default FullHeightContainer;
