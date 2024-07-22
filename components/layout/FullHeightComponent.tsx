96f3377476ac2aed8cf9617193ac8d9e9a414c3f
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-07-22 17:04:45 +0100

    fix:  removed window evaluation from component
    relates #285

--- components/layout/FullHeightComponent.tsx
+++ components/layout/FullHeightComponent.tsx
@@ -1,28 +1,11 @@
-'use client';
-
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode } from 'react';
 
 const FullHeightContainer = ({ children }: { children: ReactNode }) => {
-  const [height, setHeight] = useState(window.innerHeight);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setHeight(window.innerHeight);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  const style = {
-    height: height + 'px',
-    overflow: 'auto',
-  };
-
-  return <div style={style}>{children}</div>;
+  return (
+    <div className='h-min-[100dvh] grid grid-rows-[auto-1fr-auto]'>
+      {children}
+    </div>
+  );
 };
 
 export default FullHeightContainer;
