375c9e944b82b5eb3c6eba4b480442e236f4f108
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-08-25 17:44:00 +0100

    refactor: clear comments and redundant logic
    relates #312

--- middleware.ts
+++ middleware.ts
@@ -2,28 +2,29 @@ import { NextResponse, NextRequest } from 'next/server';
 import { createClient } from '@/supabase/utils/middleware';
 
 export async function middleware(request: NextRequest) {
-  // This `try/catch` block is only here for the interactive tutorial.
-  // Feel free to remove once you have Supabase connected.
-  const { supabase } = createClient(request);
-  // Refresh session if expired - required for Server Components
-  // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  try {
+    const { supabase } = createClient(request);
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
 
-  if (user) {
-    console.log('HAS USER');
-    const newHeaders = new Headers(request.headers);
-    newHeaders.set('k-active-user', user!.id);
+    if (user) {
+      console.log('HAS USER');
+      const newHeaders = new Headers(request.headers);
+      newHeaders.set('k-active-user', user!.id);
 
-    return NextResponse.next({
-      request: {
-        headers: newHeaders,
-      },
-    });
+      return NextResponse.next({
+        request: {
+          headers: newHeaders,
+        },
+      });
+    }
+    console.log('DOES NOT HAVE USER');
+    return NextResponse.redirect(new URL('/login', request.url));
+  } catch (error) {
+    console.error(error);
+    return NextResponse.redirect(new URL('/login', request.url));
   }
-  console.log('DOES NOT HAVE USER');
-  return NextResponse.redirect(new URL('/login', request.url));
 }
 
 export const config = {
