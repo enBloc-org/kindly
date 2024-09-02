3b694d9e6408b5bca75b8ff6f300c01f45aad79c
Olena Reukova <kronverk01@gmail.com>
2024-09-02 12:56:32 +0100

    Cleaning up the test

--- cypress/e2e/forgotPassword.cy.js
+++ cypress/e2e/forgotPassword.cy.js
@@ -4,13 +4,9 @@ import * as page from '../fixtures/URLs.json';
 describe('ForgotPassword page spec', () => {
   it('should show a validation error when the email field is left blank', () => {
     cy.visit(page.forgotPassword);
-    console.log(forgotPasswordPage);
     forgotPasswordPage.confirmResetButton().click();
     cy.get('input[name="email"]').then(($input) => {
       expect($input[0].validationMessage).to.eq('Please fill in this field.');
     });
-  });
-  it('should', () => {
-    cy.visit(page.forgotPassword);
   });
 });
