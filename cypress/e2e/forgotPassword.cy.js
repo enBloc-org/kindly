3e3a163d740bde6987ebc432a5ca8e8c51a082cf
Olena Reukova <kronverk01@gmail.com>
2024-09-02 10:43:01 +0100

    Add test for email field validation on Forgot Password page

---
+++ cypress/e2e/forgotPassword.cy.js
@@ -0,0 +1,16 @@
+import forgotPasswordPage from '../support/page_objects/forgotPasswordPage';
+import * as page from '../fixtures/URLs.json';
+
+describe('ForgotPassword page spec', () => {
+  it('should show a validation error when the email field is left blank', () => {
+    cy.visit(page.forgotPassword);
+    console.log(forgotPasswordPage);
+    forgotPasswordPage.confirmResetButton().click();
+    cy.get('input[name="email"]').then(($input) => {
+      expect($input[0].validationMessage).to.eq('Please fill in this field.');
+    });
+  });
+  it('should', () => {
+    cy.visit(page.forgotPassword);
+  });
+});
