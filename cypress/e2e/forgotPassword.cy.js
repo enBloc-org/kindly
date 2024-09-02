a36a2c3f892a80fd5f4cb81e9ef798f3d2a47b68
Olena Reukova <kronverk01@gmail.com>
2024-09-02 14:13:07 +0100

    Add the test for password reset redirection
    When the user fills in a valid email input and clicks the 'Confirm Reset' button, they are redirected to the URL /login/confirm.

--- cypress/e2e/forgotPassword.cy.js
+++ cypress/e2e/forgotPassword.cy.js
@@ -9,4 +9,11 @@ describe('ForgotPassword page spec', () => {
       expect($input[0].validationMessage).to.eq('Please fill in this field.');
     });
   });
+  it('should redirect the user to a success page when filling the valid email form and clicking "Confirm Reset" button', () => {
+    cy.visit(page.forgotPassword);
+    console.log('Login Email:', Cypress.env('loginEmail'));
+    forgotPasswordPage.emailInput().type(Cypress.env('loginEmail'));
+    forgotPasswordPage.confirmResetButton().click();
+    cy.url().should('include', '/login/confirm');
+  });
 });
