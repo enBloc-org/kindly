b8c0e13eff0a1d485fd9123ee505fd607eaee62e
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-09-07 13:57:04 +0100

    test: messaging feature e2e test suite
    relates #329

---
+++ cypress/e2e/messaging.cy.js
@@ -0,0 +1,23 @@
+import * as page from '../fixtures/URLs.json';
+import ConversationsPage from '../support/page_objects/conversationsPage';
+
+describe('messaging feature', () => {
+  beforeEach(() => {
+    cy.login(Cypress.env('loginEmail'), Cypress.env('loginPassword'));
+  });
+
+  it('alerts users for unread messages in a conversation', () => {
+    cy.visit(page.message);
+    ConversationsPage.notificationDot().should('have.length', 2);
+    ConversationsPage.conversationCard().click();
+    ConversationsPage.notificationDot().should('have.length', 1);
+    ConversationsPage.conversationCard()
+      .parents('[data-testid="card-wrapper"]')
+      .find('[name="ellipsis-button"]')
+      .click();
+    cy.get('button')
+      .contains(/mark unread/i)
+      .click();
+    ConversationsPage.notificationDot().should('have.length', 2);
+  });
+});
