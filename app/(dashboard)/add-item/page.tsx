5f5f7eedd99919066d36e2349fe89df126dcd187
Nich Galzin <nichgalzin@gmail.com>
2024-09-11 11:30:06 +0100

    refactor: moves submit logic to child component
    To avoid excess prop drilling this moves the form submit logic to the
    AddNewItemForm component
    Relates #334

--- app/(dashboard)/add-item/page.tsx
+++ app/(dashboard)/add-item/page.tsx
@@ -3,8 +3,6 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import AddNewItemForm from '@/components/form/AddNewItemForm';
 import selectLoggedUserId from '@/supabase/utils/selectLoggedUserId';
-import { PartialItem } from '@/types/supabaseTypes';
-import insertRow from '@/supabase/models/insertRow';
 
 export default function AddItemPage() {
   const [loggedUser, setLoggedUser] = useState<string | null>(null);
@@ -24,24 +22,5 @@ export default function AddItemPage() {
     getUserId();
   }, []);
 
-  const submitHandler = async (itemData: PartialItem) => {
-    try {
-      const addedItem = await insertRow('items', itemData);
-      if (!addedItem || addedItem.length === 0) {
-        throw new Error('Failed to add the new item');
-      }
-      const itemId = addedItem[0].id;
-
-      router.push(`/add-item/success/${itemId}`);
-    } catch (error) {
-      console.error('Error adding item:', error);
-      throw error;
-    }
-  };
-
-  return (
-    loggedUser && (
-      <AddNewItemForm userId={loggedUser} onSubmit={submitHandler} />
-    )
-  );
+  return loggedUser && <AddNewItemForm userId={loggedUser} />;
 }
