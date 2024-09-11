2597bc11472c3d057b54a7d2e29add0122a31aee
Nich Galzin <nichgalzin@gmail.com>
2024-09-11 11:05:37 +0100

    refactor: added dynamic role property
    Adds a dynamic role property to allow allow this button to make it more
    versatile
    Relates #334

--- components/buttons/ButtonRounded.tsx
+++ components/buttons/ButtonRounded.tsx
@@ -7,6 +7,7 @@ type ButtonRoundedPropTypes = {
   isDisabled?: boolean;
   clickHandler?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   width?: string;
+  role?: 'button' | 'submit' | 'reset';
 };
 
 const ButtonRounded: React.FC<ButtonRoundedPropTypes> = ({
@@ -15,12 +16,13 @@ const ButtonRounded: React.FC<ButtonRoundedPropTypes> = ({
   isDisabled,
   type,
   width = '',
+  role = 'button',
 }) => {
   return (
     <button
       className={`button button-rounded my-2 disabled:bg-primaryGray ${width}`}
       onClick={clickHandler}
-      role='button'
+      role={role}
       type={type}
       disabled={isDisabled}
       aria-label={`${children}`}
