276136d4f72799134476e7f2eb1fb4795e304d9b
Angelica <Angelica137@users.noreply.github.com>
2024-09-20 11:56:51 +0100

    remove redundant error

--- components/form/AddNewItemForm.tsx
+++ components/form/AddNewItemForm.tsx
@@ -15,13 +15,14 @@ export default function AddNewItemForm({
 }) {
   const [generalError, setGeneralError] = useState('');
   const [imageSource, setImageSource] = useState('');
+  const [isSubmitAttempted, setIsSubmitAttempted] = useState(false);
 
   const {
     register,
     handleSubmit,
     watch,
     reset,
-    formState: { errors },
+    formState: { isSubmitted },
   } = useForm({
     defaultValues: {
       item_name: '',
@@ -37,14 +38,13 @@ export default function AddNewItemForm({
     },
   });
 
-  const category = watch('item_type');
+  const categoryValue = watch('item_type');
   const isWillingToPostChecked = watch('postable');
   const isPickUpChecked = watch('collectible');
   const postcodeValue = watch('postcode');
   const itemNameValue = watch('item_name');
   const itemDescriptionValue = watch('item_description');
   const conditionValue = watch('condition');
-  const categoryValue = watch('item_type');
 
   const shoeSizes = ['UK3', 'UK4', 'UK5', 'UK6', 'UK7', 'UK8', 'UK10'];
   const allSizes = [
@@ -72,6 +72,11 @@ export default function AddNewItemForm({
   ];
 
   const submitHandler = async (data: PartialItem) => {
+    setIsSubmitAttempted(true);
+    if (!imageSource) {
+      setGeneralError('Please upload an image');
+      return;
+    }
     const itemData: PartialItem = {
       imageSrc: imageSource,
       donated_by: userId,
@@ -80,6 +85,8 @@ export default function AddNewItemForm({
 
     onSubmit(itemData);
     reset();
+    setIsSubmitAttempted(false);
+    setGeneralError('');
   };
 
   return (
@@ -90,28 +97,34 @@ export default function AddNewItemForm({
         onSubmit={handleSubmit(submitHandler)}
         className='flex flex-col items-center gap-5'
       >
+        {/* Item Name */}
         <label
           htmlFor='item_name'
           className='flex flex-col items-center gap-2 font-light'
         >
-          <span className='flex items-center'>Item Name</span>
+          <span className='flex items-center gap-1'>
+            Item Name
+            <span className=''>*</span>
+          </span>
           <input
             type='text'
             className='input-text'
             {...register('item_name', { required: 'This field is required' })}
           />
         </label>
-        {(!itemNameValue || errors.item_name) && (
-          <p className='error-message'>
-            {errors.item_name?.message || 'This field is required'}
-          </p>
+        {isSubmitted && !itemNameValue && (
+          <p className='error-message'>This field is required</p>
         )}
 
+        {/* Item Description */}
         <label
           htmlFor='item_description'
           className='flex flex-col items-center gap-1 font-light'
         >
-          <span className='flex items-center'>Description</span>
+          <span className='flex items-center gap-1'>
+            Description
+            <span className=''>*</span>
+          </span>
           <textarea
             {...register('item_description', {
               required: 'This field is required',
@@ -120,17 +133,19 @@ export default function AddNewItemForm({
             className='input-text'
           />
         </label>
-        {(!itemDescriptionValue || errors.item_description) && (
-          <p className='error-message'>
-            {errors.item_description?.message || 'This field is required'}
-          </p>
+        {isSubmitted && !itemDescriptionValue && (
+          <p className='error-message'>This field is required</p>
         )}
 
+        {/* Postcode */}
         <label
           htmlFor='postcode'
           className='flex flex-col items-center gap-1 font-light'
         >
-          <span className='flex items-center'>Postcode</span>
+          <span className='flex items-center gap-1'>
+            Postcode
+            <span className=''>*</span>
+          </span>
           <span className='text-xs italic'>First half</span>
           <input
             type='text'
@@ -145,147 +160,186 @@ export default function AddNewItemForm({
             className='input-text w-24 text-center'
           />
         </label>
-        {(!postcodeValue || errors.postcode) && (
-          <p className='error-message'>
-            {errors.postcode?.message || 'This field is required'}
-          </p>
+        {isSubmitted && !postcodeValue && (
+          <p className='error-message'>This field is required</p>
         )}
+
+        {/* Condition and Category */}
         <div className='mt-2 flex items-center justify-center gap-5'>
-          <label
-            htmlFor='condition'
-            className='flex flex-col items-center gap-1 font-light'
-          >
-            <span className='flex items-center'>Condition</span>
-            <select
-              {...register('condition', { required: 'Required' })}
-              className='input-text'
-            >
-              <option value='' disabled hidden>
-                Select one
-              </option>
-              <option value={'Good'}>Good</option>
-              <option value={'Fair'}>Fair</option>
-              <option value={'Poor'}>Poor</option>
-              <option value={'New'}>New</option>
-            </select>
-            {(!conditionValue || errors.condition) && (
-              <p className='error-message'>
-                {errors.condition?.message || 'This field is required'}
-              </p>
-            )}
-          </label>
-          <label
-            htmlFor='item_type'
-            className='flex flex-col items-center gap-1 font-light'
-          >
-            <span className='flex items-center'>Categories</span>
-            <select
-              {...register('item_type', { required: 'Required' })}
-              className='input-text '
-            >
-              <option value='' disabled hidden>
-                Select one
-              </option>
-              <option value={'clothing'}>Clothing</option>
-              <option value={'shoes'}>Shoes</option>
-              <option value={'toys'}>Toys</option>
-              <option value={'books'}>Books</option>
-              <option value={'household'}>Home</option>
-            </select>
-            {(!categoryValue || errors.item_type) && (
-              <p className='error-message'>
-                {errors.item_type?.message || 'This field is required'}
-              </p>
-            )}
-          </label>
-        </div>
-        {(category === 'clothing' || category === 'shoes') && (
-          <div className='flex items-center justify-center gap-5'>
+          <div className='flex flex-col items-center'>
             <label
-              htmlFor='size'
-              className='flex flex-col items-center gap-2 font-light'
+              htmlFor='condition'
+              className='flex flex-col items-center gap-1 font-light'
             >
-              Size
-              <select {...register('size')} className='input-text h-11 w-24'>
+              <span className='flex items-center gap-1'>
+                Condition
+                <span className=''>*</span>
+              </span>
+              <select
+                {...register('condition', { required: 'Required' })}
+                className='input-text'
+              >
                 <option value='' disabled hidden>
                   Select one
                 </option>
-                {category === 'clothing'
-                  ? allSizes.map((size) => (
-                      <option value={size} key={size}>
-                        {size}
-                      </option>
-                    ))
-                  : shoeSizes.map((size) => (
-                      <option value={size} key={size}>
-                        {size}
-                      </option>
-                    ))}
+                <option value={'Good'}>Good</option>
+                <option value={'Fair'}>Fair</option>
+                <option value={'Poor'}>Poor</option>
+                <option value={'New'}>New</option>
               </select>
             </label>
+            <div className='h-6'>
+              {isSubmitted && !conditionValue && (
+                <p className='error-message'>This field is required</p>
+              )}
+            </div>
+          </div>
+          <div className='flex flex-col items-center'>
             <label
               htmlFor='item_type'
               className='flex flex-col items-center gap-1 font-light'
             >
-              Gender
-              <select {...register('item_subtype')} className='input-text '>
+              <span className='flex items-center gap-1'>
+                Categories
+                <span className=''>*</span>
+              </span>
+              <select
+                {...register('item_type', { required: 'Required' })}
+                className='input-text'
+              >
                 <option value='' disabled hidden>
                   Select one
                 </option>
-                <option value={'women'}>Women</option>
-                <option value={'men'}>Men</option>
-                <option value={'girls'}>Girls</option>
-                <option value={'boys'}>Boys</option>
-                <option value={'unisex'}>Unisex</option>
+                <option value={'clothing'}>Clothing</option>
+                <option value={'shoes'}>Shoes</option>
+                <option value={'toys'}>Toys</option>
+                <option value={'books'}>Books</option>
+                <option value={'household'}>Home</option>
               </select>
-            </label>{' '}
+            </label>
+            <div className='h-6'>
+              {isSubmitted && !categoryValue && (
+                <p className='error-message'>This field is required</p>
+              )}
+            </div>
+          </div>
+        </div>
+
+        {(categoryValue === 'clothing' || categoryValue === 'shoes') && (
+          <div className='flex items-center justify-center gap-5'>
+            <div className='flex flex-col items-center'>
+              <label
+                htmlFor='size'
+                className='flex flex-col items-center gap-2 font-light'
+              >
+                Size
+                <select {...register('size')} className='input-text h-11 w-24'>
+                  <option value='' disabled hidden>
+                    Select one
+                  </option>
+                  {categoryValue === 'clothing'
+                    ? allSizes.map((size) => (
+                        <option value={size} key={size}>
+                          {size}
+                        </option>
+                      ))
+                    : shoeSizes.map((size) => (
+                        <option value={size} key={size}>
+                          {size}
+                        </option>
+                      ))}
+                </select>
+              </label>
+              <div className='h-6'></div>
+            </div>
+            <div className='flex flex-col items-center'>
+              <label
+                htmlFor='item_type'
+                className='flex flex-col items-center gap-1 font-light'
+              >
+                Gender
+                <select {...register('item_subtype')} className='input-text'>
+                  <option value='' disabled hidden>
+                    Select one
+                  </option>
+                  <option value={'women'}>Women</option>
+                  <option value={'men'}>Men</option>
+                  <option value={'girls'}>Girls</option>
+                  <option value={'boys'}>Boys</option>
+                  <option value={'unisex'}>Unisex</option>
+                </select>
+              </label>
+              <div className='h-6'></div>
+            </div>
           </div>
         )}
-        {category === 'books' && (
-          <label
-            htmlFor='item_type'
-            className='flex flex-col items-center gap-1 font-light'
-          >
-            Age
-            <select {...register('item_subtype')} className='input-text '>
-              <option value={'adults'}>Adult</option>
-              <option value={'children'}>Children</option>
-            </select>
-          </label>
+        {categoryValue === 'books' && (
+          <div className='flex flex-col items-center'>
+            <label
+              htmlFor='item_type'
+              className='flex flex-col items-center gap-1 font-light'
+            >
+              Age
+              <select {...register('item_subtype')} className='input-text'>
+                <option value={'adults'}>Adult</option>
+                <option value={'children'}>Children</option>
+              </select>
+            </label>
+            <div className='h-6'></div>
+          </div>
         )}
-        <div className='mt-5 flex flex-col gap-3'>
-          <label className='flex items-center gap-2 font-light'>
-            <input type='checkbox' {...register('postable')} className='mr-2' />
-            Willing to Post
-          </label>
-          <label className='flex items-center gap-2 font-light'>
-            <input
-              type='checkbox'
-              {...register('collectible')}
-              className='mr-2'
-            />
-            Pick Up
-          </label>
-          <label className='flex items-center justify-center gap-2 font-light'>
-            <input
-              type='checkbox'
-              {...register('postage_covered')}
-              className='mr-2'
-            />
-            Postage Covered
-          </label>
+
+        {/* Delivery Options */}
+        <div className='mt-5 flex flex-col items-center gap-3'>
+          <span className='flex items-center gap-1 font-light'>
+            Delivery Options
+            <span className=''>*</span>
+          </span>
+          <div className='flex flex-col gap-3'>
+            <label className='flex items-center gap-2 font-light'>
+              <input
+                type='checkbox'
+                {...register('postable')}
+                className='mr-2'
+              />
+              Willing to Post
+            </label>
+            <label className='flex items-center gap-2 font-light'>
+              <input
+                type='checkbox'
+                {...register('collectible')}
+                className='mr-2'
+              />
+              Pick Up
+            </label>
+            <label className='flex items-center gap-2 font-light'>
+              <input
+                type='checkbox'
+                {...register('postage_covered')}
+                className='mr-2'
+              />
+              Postage Covered
+            </label>
+          </div>
         </div>
-        {!isPickUpChecked && !isWillingToPostChecked && (
-          <p className='error-message'>Select at least one option </p>
-        )}
+        <div className='h-6'>
+          {isSubmitted && !isPickUpChecked && !isWillingToPostChecked && (
+            <p className='error-message'>Select at least one option</p>
+          )}
+        </div>
 
         <UploadImageInput
           setImageSrc={setImageSource}
           setError={setGeneralError}
           isRequired={true}
           imageType={'item'}
+          showErrorMessage={false}
         />
-        {generalError && <p className='error-message'>{generalError}</p>}
+        {isSubmitAttempted && !imageSource && (
+          <p className='error-message'>
+            {generalError || 'Please upload an image'}
+          </p>
+        )}
 
         <ButtonRounded type='submit'>ADD YOUR ITEM</ButtonRounded>
       </form>
