dd596dccac7c234523c25d99e6d9bb3be5ea2627
AnyaVeselova <anna.correspondence@outlook.com>
2024-09-23 21:28:58 +0100

    changes any type to PartialItem

--- components/form/AddNewItemForm.tsx
+++ components/form/AddNewItemForm.tsx
@@ -2,7 +2,7 @@
 import { useForm } from 'react-hook-form';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-
+import SizeSelector from './SizeSelector';
 import ButtonRounded from '../buttons/ButtonRounded';
 import UploadImageInput from './UploadImageInput';
 import { PartialItem } from '@/types/supabaseTypes';
@@ -41,31 +41,6 @@ export default function AddNewItemForm({
   const category = watch('item_type');
   const isWillingToPostChecked = watch('postable');
   const isPickUpChecked = watch('collectible');
-
-  const shoeSizes = ['UK3', 'UK4', 'UK5', 'UK6', 'UK7', 'UK8', 'UK10'];
-  const allSizes = [
-    'XXS',
-    'XS',
-    'S',
-    'M',
-    'L',
-    'XL',
-    'XXL',
-    'XXXL',
-    'UK4',
-    'UK5',
-    'UK6',
-    'UK8',
-    'UK10',
-    'UK12',
-    'UK14',
-    'UK16',
-    'UK18',
-    'UK20',
-    'UK22',
-    'UK24',
-    'UK26',
-  ];
 
   const submitHandler = async (data: PartialItem) => {
     const itemData: PartialItem = {
@@ -180,28 +155,7 @@ export default function AddNewItemForm({
         </div>
         {(category === 'clothing' || category === 'shoes') && (
           <div className='flex items-center justify-center gap-5'>
-            <label
-              htmlFor='size'
-              className='flex flex-col items-center gap-2 font-light'
-            >
-              Size
-              <select {...register('size')} className='input-text h-11 w-24'>
-                <option value='' disabled hidden>
-                  Select one
-                </option>
-                {category === 'clothing'
-                  ? allSizes.map((size) => (
-                      <option value={size} key={size}>
-                        {size}
-                      </option>
-                    ))
-                  : shoeSizes.map((size) => (
-                      <option value={size} key={size}>
-                        {size}
-                      </option>
-                    ))}
-              </select>
-            </label>
+            <SizeSelector category={category} register={register} />
             <label
               htmlFor='item_type'
               className='flex flex-col items-center gap-1 font-light'
