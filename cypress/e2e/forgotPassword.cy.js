f119c67e57500f5ca7edc65955ec660d683bc3cc
Olena Reukova <kronverk01@gmail.com>
2024-09-25 19:09:27 +0100

    test:resetPasswordPage

--- cypress/e2e/forgotPassword.cy.js
+++ cypress/e2e/forgotPassword.cy.js
@@ -1,4 +1,5 @@
 import forgotPasswordPage from '../support/page_objects/forgotPasswordPage';
+import resetPasswordPage from '../support/page_objects/resetPasswordPage';
 import * as page from '../fixtures/URLs.json';
 
 describe('ForgotPassword page spec', () => {
@@ -33,5 +34,53 @@ describe('ForgotPassword page spec', () => {
       'contain.text',
       `Check your email. Password reset link has been sent to ${testEmail}`
     );
+  });
+});
+
+describe('Reset Password Page', () => {
+  const newPassword = 'Password123!';
+  const confirmPassword = 'Password123!';
+  const mismatchedPassword = '321Password!';
+  const errorMessageMatch = 'Passwords do not match!';
+
+  it('should display the reset password form', () => {
+    cy.visit(page.resetPassword);
+    resetPasswordPage.passwordInput().should('exist').should('be.visible');
+    resetPasswordPage
+      .confirmPasswordInput()
+      .should('exist')
+      .should('be.visible');
+    resetPasswordPage.resetButton().should('exist').should('be.visible');
+  });
+  it('should allow typing in the password fields', () => {
+    cy.visit(page.resetPassword);
+    resetPasswordPage.passwordInput().type(newPassword);
+    resetPasswordPage.confirmPasswordInput().type(confirmPassword);
+    resetPasswordPage.passwordInput().should('have.value', newPassword);
+    resetPasswordPage
+      .confirmPasswordInput()
+      .should('have.value', confirmPassword);
+  });
+  it('should display an error when passwords do not match', () => {
+    cy.visit(page.resetPassword);
+    resetPasswordPage.passwordInput().type(newPassword);
+    resetPasswordPage.confirmPasswordInput().type(mismatchedPassword);
+    resetPasswordPage.resetButton().click();
+    resetPasswordPage.message().should('contain.text', errorMessageMatch);
+  });
+  it('should show error if password is left blank', () => {
+    cy.visit(page.resetPassword);
+    resetPasswordPage.passwordInput().should('exist').should('be.visible');
+    resetPasswordPage
+      .confirmPasswordInput()
+      .should('exist')
+      .should('be.visible');
+    resetPasswordPage.confirmPasswordInput().type(confirmPassword);
+    resetPasswordPage.resetButton().click();
+    cy.get('input[name="password"]').then(($input) => {
+      expect($input[0].validationMessage).to.match(
+        /Please fill in this field./gi
+      );
+    });
   });
 });
