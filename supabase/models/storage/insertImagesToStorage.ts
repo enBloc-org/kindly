b169b57709c3c81ecc79c8f907a5d98804d5c9d5
Nich Galzin <nichgalzin@gmail.com>
2024-12-16 17:49:43 +0000

    refactor: improved upload handler and return type
    Images are now uploaded to a sub-directory and a further sub-directory
    per item. Return type now returns an array of correctly constructed urls
    in the data property.

--- supabase/models/storage/insertImagesToStorage.ts
+++ supabase/models/storage/insertImagesToStorage.ts
@@ -3,47 +3,58 @@ import newClient from '@/supabase/utils/newClient';
 import { deleteExistingProfileImage } from './deleteExistingProfileImage';
 
 type SupabaseStorageResponse = {
-  data: {
-    path: string;
-  } | null;
+  data: string[] | null;
   error: unknown;
+};
+
+const getFullImageUrl = (path: string) => {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  return `${supabaseUrl}/storage/v1/object/public/images/${path}`;
 };
 
 const insertImagesToStorage = async (
   files: File[] | Blob[],
   imageType: 'item' | 'profile',
   userId: string
-): Promise<SupabaseStorageResponse[]> => {
+): Promise<SupabaseStorageResponse> => {
   const supabase = newClient();
+  const folderUuid = uuidv4();
 
   try {
     if (imageType === 'profile') {
-      const { error: deleteError } = await deleteExistingProfileImage(supabase, userId);
+      const { error: deleteError } = await deleteExistingProfileImage(
+        supabase,
+        userId
+      );
       if (deleteError) {
-        return [{ data: null, error: deleteError }];
+        return { data: null, error: deleteError };
       }
     }
 
     const uploadPromises = files.map(async (file) => {
-      const prefix = imageType === 'profile' ? 'profile_' : 'item_';
-      const imageName = prefix + uuidv4();
+      const fileUuid = uuidv4();
+      const filePath =
+        imageType === 'item'
+          ? `${userId}/${imageType}/${folderUuid}/${fileUuid}`
+          : `${userId}/${folderUuid}/${fileUuid}`;
 
       const { data, error: uploadError } = await supabase.storage
         .from('images')
-        .upload(userId + '/' + imageName, file);
+        .upload(filePath, file);
 
       if (uploadError) {
         console.error('Error uploading image:', uploadError);
-        return { data: null, error: uploadError };
+        throw uploadError;
       }
 
-      return { data: { path: data.path }, error: null };
+      return getFullImageUrl(data.path);
     });
 
-    return await Promise.all(uploadPromises);
+    const paths = await Promise.all(uploadPromises);
+    return { data: paths, error: null };
   } catch (error) {
     console.error('Error handling image uploads:', error);
-    return [{ data: null, error }];
+    return { data: null, error };
   }
 };
 
