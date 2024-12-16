d7e08858c401059b641c89bcc1c63ce87d0e7003
Nich Galzin <nichgalzin@gmail.com>
2024-12-16 17:51:47 +0000

    test: adds play test for filtering functionality
    Relates #334

--- components/search/filter/CategoryButtonContainer.stories.tsx
+++ components/search/filter/CategoryButtonContainer.stories.tsx
@@ -1,7 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { SearchParamsType } from '@/types/searchPageTypes';
+import { useState } from 'react';
+import {
+  ApparelSubcategoryType,
+  BooksSubcategoryType,
+  CategoryType,
+  SearchParamsType,
+} from '@/types/searchPageTypes';
 import CategoryButtonContainer from './CategoryButtonContainer.';
-import { fn } from '@storybook/test';
+import { userEvent, within, waitFor, fn } from '@storybook/test';
+import { expect } from '@storybook/jest';
 
 const meta: Meta<typeof CategoryButtonContainer> = {
   title: 'Search/CategoryButtons',
@@ -34,6 +41,63 @@ export const CategoryButtonsDefault: Story = {
     handleSubcategoryClick: fn(),
     handleFilterApply: fn(),
     handleFilterClear: fn(),
+  },
+  render: (args) => {
+    const [searchParams, setSearchParams] = useState(args.searchParams);
+
+    const handleCategoryClick = (category: CategoryType) => {
+      setSearchParams((prevParams) => ({
+        ...prevParams,
+        category,
+        subcategory: '',
+      }));
+    };
+
+    const handleSubcategoryClick = (
+      subcategory: BooksSubcategoryType | ApparelSubcategoryType
+    ) => {
+      setSearchParams((prevParams) => ({
+        ...prevParams,
+        subcategory,
+      }));
+    };
+
+    const handleFilterClear = () => {
+      setSearchParams({
+        category: '',
+        subcategory: '',
+      });
+    };
+
+    return (
+      <CategoryButtonContainer
+        {...args}
+        searchParams={searchParams}
+        handleCategoryClick={handleCategoryClick}
+        handleSubcategoryClick={handleSubcategoryClick}
+        handleFilterClear={handleFilterClear}
+      />
+    );
+  },
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+    await step('Click Clothing Button', async () => {
+      await userEvent.click(canvas.getByText('Clothing'));
+    });
+
+    await waitFor(() => {
+      expect(canvas.getByText('Men')).toBeVisible();
+      expect(canvas.getByText('Women')).toBeVisible();
+    });
+
+    await step('Click the clear filter button', async () => {
+      await userEvent.click(canvas.getByText('CLEAR FILTERS'));
+    });
+
+    await waitFor(() => {
+      expect(canvas.queryByText('Men')).not.toBeInTheDocument();
+      expect(canvas.queryByText('Women')).not.toBeInTheDocument();
+    });
   },
 };
 
