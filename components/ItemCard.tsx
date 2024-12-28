6649f5f9014c57c3b172f282fb3e713e3a1e15c8
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-12-28 16:07:58 +0000

    refactor: edit prop type to especifically target the new set of data to be displayed in the re-designed card
    relates #393

--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -3,45 +3,56 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 //Types
-import { PartialItem } from '@/types/supabaseTypes';
+import { item } from '@/types/supabaseTypes';
 
-//Components
-import ItemDetails from './ItemDetails';
-
-const ItemCard: React.FC<PartialItem> = ({
-  imageSrc,
-  item_name,
-  condition,
-  postcode,
-  size,
-  postage_covered,
+const ItemCard: React.FC<
+  Pick<
+    item,
+    | 'id'
+    | 'item_name'
+    | 'size'
+    | 'postcode'
+    | 'imageSrc'
+    | 'postable'
+    | 'collectible'
+    | 'postage_covered'
+  >
+> = ({
   id,
-  is_reserved,
-  given_away_to,
+  item_name,
+  size,
+  postcode,
+  imageSrc,
+  postable,
+  collectible,
+  postage_covered,
 }) => {
+  const displayDeliveryOptions = () => {
+    switch (true) {
+      case postable && !collectible && !postage_covered:
+        return 'Posting';
+        break;
+      case collectible && !postable && !postage_covered:
+        return 'Pick-up only';
+        break;
+      case postage_covered && !collectible:
+        return 'Posting (postage covered)';
+        break;
+      case postable && collectible && !postage_covered:
+        return 'Pick-up or posting';
+        break;
+      case postable && collectible && postage_covered:
+        return 'Pick-up or posting (postage covered)';
+        break;
+    }
+  };
+
   return (
     <Link href={`/item/${id}`}>
       <div
         className='card relative m-auto mt-8
         flex h-[400px] w-[176px] flex-col gap-3 sm:w-[200px] md:h-[450px] md:w-[256px]'
       >
-        {given_away_to ? (
-          <div
-            className='absolute left-0 right-0 top-14 z-10 m-auto w-[175px] rounded-lg border border-primaryGreen
-            bg-background p-2 text-center opacity-70 md:w-[200px]'
-          >
-            <p className='text-lg text-primaryGreen'>GIVEN AWAY</p>
-          </div>
-        ) : (
-          is_reserved && (
-            <div
-              className='absolute left-0 right-0 top-14 z-10 m-auto w-[175px] rounded-lg border border-primaryGreen
-            bg-background p-2 text-center opacity-70 md:w-[200px]'
-            >
-              <p className='text-lg text-primaryGreen'>RESERVED</p>
-            </div>
-          )
-        )}
         <div className='relative h-[176px] w-[176px] sm:w-[200px] md:h-52 md:w-64'>
           <Image
             src={imageSrc ? `${imageSrc}` : '/default-item-img.png'}
@@ -50,16 +61,25 @@ const ItemCard: React.FC<PartialItem> = ({
             sizes='(max-width: 768px) 100vw, 50vw'
           />
         </div>
-        <h2 className='p-4 text-center font-semibold md:text-xl'>
-          {item_name}
-        </h2>
-        <ItemDetails
-          condition={condition}
-          size={size}
-          postcode={postcode}
-          postage_covered={postage_covered}
-          fontSize='text-md'
-        />
+        <div className='flex flex-col text-start'>
+          <h2 className='text-xl'>
+            <b>{item_name}</b>
+          </h2>
+          <p>
+            <b>Size:</b>
+            {size}
+          </p>
+          <p>
+            <b>Postcode:</b>
+            {postcode}
+          </p>
+          <p>
+            <b>Delivery Preferences:</b>
+            <span className='text-primaryOrange'>
+              {displayDeliveryOptions()}
+            </span>
+          </p>
+        </div>
       </div>
     </Link>
   );
