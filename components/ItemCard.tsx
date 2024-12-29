b4b0668b12a32f83e1efc23a53353ebc15b27e22
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-12-29 16:37:05 +0000

    style: basic restyling to match the new figma design
    relates #393

--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
 //Types
 import { item } from '@/types/supabaseTypes';
+import startNewConversation from '@/supabase/models/messaging/startNewConversation';
 
 const ItemCard: React.FC<
   Pick<
@@ -17,7 +18,8 @@ const ItemCard: React.FC<
     | 'collectible'
     | 'postage_covered'
     | 'created_at'
-  >
+    | 'donated_by'
+  > & { userId: string }
 > = ({
   id,
   item_name,
@@ -28,34 +30,42 @@ const ItemCard: React.FC<
   collectible,
   postage_covered,
   created_at,
+  donated_by,
+  userId,
 }) => {
+  const [message, setMessage] = useState<string>('');
+
   const displayDeliveryOptions = () => {
     switch (true) {
       case postable && !collectible && !postage_covered:
         return 'Posting';
-        break;
       case collectible && !postable && !postage_covered:
         return 'Pick-up only';
-        break;
       case postage_covered && !collectible && postable:
         return 'Posting (postage covered)';
-        break;
       case postable && collectible && !postage_covered:
         return 'Pick-up or posting';
-        break;
       case postable && collectible && postage_covered:
         return 'Pick-up or posting (postage covered)';
-        break;
+    }
+  };
+
+  const messageButtonHandler = async () => {
+    try {
+      await startNewConversation(userId, donated_by, id);
+    } catch (error) {
+      setMessage('Something went wrong. Please try again later.');
+      console.error(error);
     }
   };
 
   return (
     <Link href={`/item/${id}`}>
       <div
-        className='card relative m-auto mt-8
-        flex h-[400px] w-[176px] flex-col gap-3 sm:w-[200px] md:h-[450px] md:w-[256px]'
+        className='card m-auto mt-8
+        flex h-[568px] w-[350px] flex-col justify-between p-0 sm:w-[200px] md:h-[450px] md:w-[256px]'
       >
-        <div className='relative h-[176px] w-[176px] sm:w-[200px] md:h-52 md:w-64'>
+        <div className='relative h-[334.53px] w-[350px] sm:w-[200px] md:h-52 md:w-64'>
           <Image
             src={imageSrc ? `${imageSrc}` : '/default-item-img.png'}
             alt={`Image of ${item_name}`}
@@ -63,31 +73,35 @@ const ItemCard: React.FC<
             sizes='(max-width: 768px) 100vw, 50vw'
           />
         </div>
-        <div className='flex flex-col text-start'>
+        <div className='flex h-[137px] w-[350px] flex-col justify-between text-start'>
           <p className='text-sm text-primaryGray'>
             Date added:{' '}
             <span>
-              {created_at.slice(0, 10).split('-').toReversed().join('-')}
+              {created_at.slice(0, 10).split('-').toReversed().join('.')}
             </span>
           </p>
-          <h2 className='text-xl'>
+          <p className='text-[2rem]'>
             <b>{item_name}</b>
-          </h2>
+          </p>
           <p className='text-sm'>
-            <b>Size:</b>
+            <b>Size: </b>
             {size}
           </p>
           <p className='text-sm'>
-            <b>Postcode:</b>
+            <b>Postcode: </b>
             {postcode}
           </p>
           <p className='text-sm'>
-            <b>Delivery Preferences:</b>
-            <span className='text-primaryOrange'>
+            <b>Delivery Preferences: </b>
+            <span className='font-semibold text-primaryOrange'>
               {displayDeliveryOptions()}
             </span>
           </p>
         </div>
+        {message.length > 0 && <p className='error-message'>{message}</p>}
+        <button className='button' onClick={messageButtonHandler}>
+          MESSAGE
+        </button>
       </div>
     </Link>
   );
