cb0cd6c624f34188039cd15aec03491950b66ac2
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-12-29 15:02:25 +0000

    test: adjust stories for new prop list
    relates #393

--- components/ItemCard.stories.ts
+++ components/ItemCard.stories.ts
@@ -8,49 +8,33 @@ import testItem from '../public/white-jumper.jpg';
 const meta: Meta<typeof ItemCard> = {
   title: 'Donations/ItemCard',
   component: ItemCard,
+  args: {
+    imageSrc: testItem.src,
+    item_name: 'White Jumper',
+    size: 'Medium',
+    postcode: 'E12',
+    postable: true,
+    id: 2,
+    collectible: true,
+    postage_covered: false,
+    created_at: '2024-06-16 13:34:25.931798+00',
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const ItemCardDefault: Story = {
-  args: {
-    imageSrc: testItem.src,
-    item_name: 'White Jumper',
-    condition: 'New',
-    size: 'Medium',
-    postcode: 'E12',
-    postable: true,
-    id: 2,
-    is_reserved: false,
-  },
-};
+export const ItemCardDefault: Story = {};
 
 export const ItemCardPotageCovered: Story = {
   args: {
-    imageSrc: testItem.src,
-    item_name: 'White Jumper',
-    condition: 'New',
-    size: 'Medium',
-    postcode: 'E12',
-    postable: true,
-    id: 2,
-    is_reserved: false,
+    collectible: false,
     postage_covered: true,
   },
 };
 
 export const ItemCardReserved: Story = {
-  args: {
-    imageSrc: testItem.src,
-    item_name: 'White Jumper',
-    condition: 'New',
-    size: 'Medium',
-    postcode: 'E12',
-    postable: true,
-    id: 2,
-    is_reserved: true,
-  },
+  args: {},
   play: async ({ canvasElement, step }) => {
     const canvas = within(canvasElement);
 
