19191853867f73f953db9c73fb8c4811bc26dac9
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2025-01-01 11:35:05 +0000

    test: extend searchPage class to return any select item card in tests
    relates #393

--- cypress/support/page_objects/searchPage.js
+++ cypress/support/page_objects/searchPage.js
@@ -16,6 +16,10 @@ export class SearchPage extends BasePage {
   firstSearchResult() {
     return cy.get('div.card').first().should('be.visible');
   }
+
+  selectItemCard(itemName) {
+    return cy.get('div.card').contains(itemName);
+  }
 }
 
 export default new SearchPage();
