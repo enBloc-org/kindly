be46106931579eff4d174f13f76d280ba1968593
Nich Galzin <nichgalzin@gmail.com>
2025-01-28 11:01:42 +0000

    refactor: removes unused props
    relates #399

--- components/navigation/NavigationLinkContainer.tsx
+++ components/navigation/NavigationLinkContainer.tsx
@@ -4,31 +4,17 @@ import { ReactNode } from 'react';
 type NavigationLinkContainerProps = {
   href: string;
   ariaLabel: string;
-  pathName: string;
   children: ReactNode;
-  size: 'mobile' | 'desktop';
 };
 
 const NavigationLinkContainer: React.FC<NavigationLinkContainerProps> = ({
   href,
   ariaLabel,
-  pathName,
   children,
-  size,
 }) => {
   return (
     <Link href={href} aria-label={ariaLabel}>
-      <div
-        className={`relative flex flex-col items-center justify-center ${
-          size === 'mobile'
-            ? pathName === href
-              ? 'rounded-full border-4 border-primaryOrange bg-backgroundHighlight p-1'
-              : 'rounded-full border-4 border-primaryGreen p-1'
-            : pathName === href
-              ? ' rounded-lg bg-backgroundHighlight p-2'
-              : 'rounded-lg p-2'
-        }`}
-      >
+      <div className={`relative flex flex-col items-center justify-center`}>
         {children}
       </div>
     </Link>
