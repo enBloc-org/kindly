8aee634577e9962a0321efc1cc6f1e5bfe493417
Nich Galzin <nichgalzin@gmail.com>
2025-03-03 15:07:03 +0000

    feat: component for hamburger menu
    Relates #399

--- components/menus/HamburgerMenu.tsx
+++ components/menus/HamburgerMenu.tsx
@@ -1,85 +1,103 @@
 import Link from 'next/link';
+import { Dispatch, SetStateAction } from 'react';
 import HomeRouteIcon from '../icons/navigation/HomeRouteIcon';
 import SearchRouteIcon from '../icons/navigation/SearchRouteIcon';
 import AddItemRouteIcon from '../icons/navigation/AddItemRouteIcon';
 import MessageRouteIcon from '../icons/navigation/MessageRouteIcon';
 import ProfileRouteIcon from '../icons/navigation/ProfileRouteIcon';
-import { Dispatch, SetStateAction } from 'react';
+import MainButton from '../buttons/MainButton/MainButton';
+import HamburgerIcon from '../icons/navigation/HamburgerIcon';
 
 type HamburgerMenuProps = {
-  menuOpen: boolean;
-  setMenuOpen: Dispatch<SetStateAction<boolean>>;
+  isMenuOpen: boolean;
+  setIsMenuOpen: Dispatch<SetStateAction<boolean>>;
+  userId: string | null;
 };
 
 const HamburgerMenu: React.FC<HamburgerMenuProps> = ({
-  menuOpen,
-  setMenuOpen,
+  isMenuOpen,
+  setIsMenuOpen,
+  userId,
 }) => {
+  const clickHandler = () => setIsMenuOpen(false);
+
   return (
     <>
       <nav className='relative' role='menu'>
         <button
           className='p-[.1rem]'
           aria-label='Menu button'
-          onClick={() => setMenuOpen((prev) => !prev)}
+          aria-expanded={isMenuOpen}
+          onClick={() => setIsMenuOpen((prev) => !prev)}
         >
-          <svg
-            width='42'
-            height='42'
-            viewBox='0 0 42 42'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              d='M13 15H29'
-              stroke='#333333'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            />
-            <path
-              d='M13 21H29'
-              stroke='#333333'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            />
-            <path
-              d='M13 27H29'
-              stroke='#333333'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            />
-          </svg>
+          <HamburgerIcon width={42} height={42} />
         </button>
       </nav>
-      {menuOpen && (
-        <div className='absolute left-0 top-[64px] z-50 h-full w-full bg-monoY px-2 py-6'>
-          <Link href='/' className='menu-link'>
+      {isMenuOpen && (
+        <div className={isMenuOpen ? 'menu-open' : 'menu-close'}>
+          {!userId && (
+            <div className='mb-4 flex w-full flex-col items-center justify-center gap-4'>
+              <Link href='/signup'>
+                <MainButton
+                  size='large'
+                  type='button'
+                  styling='w-full min-w-[325px]'
+                  clickHandler={clickHandler}
+                >
+                  Sign up
+                </MainButton>
+              </Link>
+              <Link href='/login'>
+                <MainButton
+                  size='large'
+                  type='button'
+                  variant='secondary'
+                  styling='w-full min-w-[325px]'
+                  clickHandler={clickHandler}
+                >
+                  Log in
+                </MainButton>
+              </Link>
+            </div>
+          )}
+          <Link href='/' className='menu-link' onClick={clickHandler}>
             <HomeRouteIcon height={42} width={42} />
             Home
           </Link>
-          <Link href='/search' className='menu-link'>
+          <Link href='/search' className='menu-link' onClick={clickHandler}>
             <SearchRouteIcon height={42} width={42} />
             Search Items
           </Link>
-          <Link href='/add-item' className='menu-link'>
+          <Link href='/add-item' className='menu-link' onClick={clickHandler}>
             <AddItemRouteIcon height={42} width={42} />
             Post an Item
           </Link>
-          <Link href='/conversations' className='menu-link'>
+          <Link
+            href='/conversations'
+            className='menu-link'
+            onClick={clickHandler}
+          >
             <MessageRouteIcon height={42} width={42} />
             Messages
           </Link>
-          <Link href='/profile' className='menu-link last-link'>
+          <Link
+            href='/profile'
+            className='menu-link last-link'
+            onClick={clickHandler}
+          >
             <ProfileRouteIcon height={42} width={42} />
             Profile
           </Link>
           <div className='mt-4 flex flex-col gap-4 px-4'>
-            <Link href='/about'>About Kyndly</Link>
-            <Link href='/about'>Frequently Asked Questions</Link>
-            <Link href='/profile'>Contacts</Link>
+            <Link href='/about' onClick={clickHandler}>
+              About Kyndly
+            </Link>
+            <Link href='/about' onClick={clickHandler}>
+              Frequently Asked Questions
+            </Link>
+            <Link href='/about' onClick={clickHandler}>
+              Contacts
+            </Link>
           </div>
         </div>
       )}
