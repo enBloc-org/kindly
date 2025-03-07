7db134422d279f4b21b51fb3c5ceba6445ec261e
Nich Galzin <nichgalzin@gmail.com>
2025-03-07 16:10:09 +0000

    fix: fixes page component to use new context and avoid another db call to get user data
    Relates #399

--- app/(dashboard)/profile/page.tsx
+++ app/(dashboard)/profile/page.tsx
@@ -1,62 +1,53 @@
-import { headers } from 'next/headers';
-
-import { getProfile } from '@/supabase/models/getProfile';
+'use client';
 import LogOutButton from '@/components/LogOutButton';
 import { ProfileEdit } from '@/components/form/ProfileEdit';
 import DonatedItemsList from '@/components/DonatedItemsList';
+import { useUser } from '@/context/UserProvider';
 
 const ProfilePage = async () => {
-  const headersList = headers();
-  const userId = headersList.get('k-active-user')!;
+  const { user } = useUser();
+  if (!user) {
+    return <div>Error User profile not found or username is missing</div>;
+  }
 
-  try {
-    const { data: userProfile } = await getProfile(userId);
-
-    if (!userProfile) {
-      return <div>Error User profile not found or username is missing</div>;
-    }
-
-    return (
-      <>
-        <div className='mt-10 flex items-center justify-between px-5 md:px-20 lg:px-40'>
-          <div className='px-5 py-2'>
-            <h1 className='pl-3 text-2xl'>Profile</h1>
-            <div className='mt-2 flex gap-3'>
-              <h2 className='italic'>{userProfile.username}</h2>
-              <LogOutButton>LOG OUT</LogOutButton>
-            </div>
-          </div>
-          <div className='mt-10 flex flex-col items-center justify-between gap-4 px-4'>
-            {userProfile?.avatar ? (
-              <img
-                src={userProfile.avatar}
-                alt='User avatar'
-                width={100}
-                height={100}
-                className='rounded-full'
-              />
-            ) : (
-              <img
-                src='/default-profile.png'
-                alt='Default avatar'
-                width={100}
-                height={100}
-                className='rounded-full'
-              />
-            )}
-            <ProfileEdit
-              userId={userProfile.id}
-              userName={userProfile.username}
-              userAvatar={userProfile.avatar}
-            />
+  return (
+    <>
+      <div className='mt-10 flex items-center justify-between px-5 md:px-20 lg:px-40'>
+        <div className='px-5 py-2'>
+          <h1 className='pl-3 text-2xl'>Profile</h1>
+          <div className='mt-2 flex gap-3'>
+            <h2 className='italic'>{user.username}</h2>
+            <LogOutButton>LOG OUT</LogOutButton>
           </div>
         </div>
-        <DonatedItemsList userId={userProfile.id} />
-      </>
-    );
-  } catch (error) {
-    return <div>An error has occurred</div>;
-  }
+        <div className='mt-10 flex flex-col items-center justify-between gap-4 px-4'>
+          {user?.avatar ? (
+            <img
+              src={user.avatar}
+              alt='User avatar'
+              width={100}
+              height={100}
+              className='rounded-full'
+            />
+          ) : (
+            <img
+              src='/default-profile.png'
+              alt='Default avatar'
+              width={100}
+              height={100}
+              className='rounded-full'
+            />
+          )}
+          <ProfileEdit
+            userId={user.id}
+            userName={user.username}
+            userAvatar={user.avatar}
+          />
+        </div>
+      </div>
+      <DonatedItemsList userId={user.id} />
+    </>
+  );
 };
 
 export default ProfilePage;
