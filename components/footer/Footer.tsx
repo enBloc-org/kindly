38e4d3b2bbfd524fd567414cbd66b91b4d9446da
Olena Reukova <34659641+OlenaReukova@users.noreply.github.com>
2025-03-07 11:25:21 +0000

    refactor: add flex and justify-between to align items along the container's main axis, ensuring equal spacing between each item

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -11,9 +11,9 @@ const Footer: React.FC<FooterProps> = ({
   socialLinks = defaultSocialLinks,
 }) => {
   return (
-    <footer className='mt-auto w-full bg-brand-80 text-gray-800'>
-      <div className='mx-auto max-w-7xl  px-4 py-8'>
-        <div className='hidden md:grid md:grid-cols-4 md:gap-8'>
+    <footer className='mt-auto w-full bg-brand-80 text-gray-800  '>
+      <div className='mx-auto max-w-7xl  px-4 py-8 '>
+        <div className='flex justify-between'>
           <div>
             <h3 className='mb-4 font-bold'>Follow us at:</h3>
             <div className='flex space-x-4'>
