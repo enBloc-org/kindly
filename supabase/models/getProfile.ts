58f558c8fb171e36642587e35861199152a3528b
Nich Galzin <nichgalzin@gmail.com>
2025-03-07 15:49:53 +0000

    fix: updates function to have a better error handling and data return
    Relates #399

--- supabase/models/getProfile.ts
+++ supabase/models/getProfile.ts
@@ -1,17 +1,20 @@
 'use server';
+import { profile } from '@/types/supabaseTypes';
 import newServerClient from '../utils/newServerClient';
 
-export async function getProfile(id: string | undefined) {
+export async function getProfile(id: string | undefined): Promise<profile> {
   const supabase = newServerClient();
 
   try {
-    const userProfile = await supabase
+    const { data, error } = await supabase
       .from('profiles')
       .select('*')
       .eq('id', id)
       .single();
+    if (error)
+      throw new Error(`Error updating user_conversations: ${error.message}`);
 
-    return userProfile;
+    return data;
   } catch (error) {
     console.error(`Failed to fetch conversations from database: ${error}`);
     throw error;
