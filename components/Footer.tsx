aad018502d99321b1960e31b150095b2946e0443
Nich Galzin <nichgalzin@gmail.com>
2025-03-10 15:30:03 +0000

    fix: removes old navigation
    Relates #399

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,5 @@
 'use client';
-import MobileNavbar from './navigation/MobileNavbar';
-import useMediaQuery from './hooks/useMediaQuery';
+// import useMediaQuery from './hooks/useMediaQuery';
 import { useConversationContext } from '@/context/conversationContext';
 
 const Footer = () => {
@@ -12,9 +11,7 @@ const Footer = () => {
   return (
     isBreakpoint &&
     showConversationsList && (
-      <footer className='fixed bottom-0 z-20 h-auto w-full bg-background py-2'>
-        <MobileNavbar />
-      </footer>
+      <footer className='fixed bottom-0 z-20 h-auto w-full bg-background py-2'></footer>
     )
   );
 };
