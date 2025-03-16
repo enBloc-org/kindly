f87c81bdc13f2b8bf792fde33bb8f2432cfddc40
Olena Reukova <34659641+OlenaReukova@users.noreply.github.com>
2025-03-16 16:23:00 +0000

    fix:mobile view

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -12,9 +12,9 @@ const Footer: React.FC<FooterProps> = ({
   socialLinks = defaultSocialLinks,
 }) => {
   return (
-    <footer className='mt-auto w-full bg-brand-80 text-gray-800  '>
-      <div className='mx-auto max-w-7xl  px-4 py-8 '>
-        <div className='flex justify-between'>
+    <footer className='mt-auto w-full bg-brand-80 text-gray-800'>
+      <div className='mx-auto max-w-7xl px-4 py-8'>
+        <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4'>
           <div>
             <h3 className='mb-4 font-bold'>Follow us at:</h3>
             <div className='flex space-x-4'>
@@ -47,21 +47,21 @@ const Footer: React.FC<FooterProps> = ({
                   href={socialLinks.linkedin}
                   className='text-gray-600 hover:text-gray-900'
                 >
-                  <LinkedInIcon className='h-6 w-6 ' />
+                  <LinkedInIcon className='h-6 w-6' />
                 </Link>
               )}
             </div>
-            <div className='mt-4'>
+            <div className='mt-4 text-sm'>
               <p className='font-bold'>{companyInfo.name}</p>
-              <p className='text-sm'>
+              <p>
                 UK Registered Company Number {companyInfo.registrationNumber}
               </p>
-              <p className='mt-2 text-sm font-bold'>Registered Address</p>
-              <p className='text-sm'>{companyInfo.address}</p>
+              <p className='mt-2 font-bold'>Registered Address</p>
+              <p>{companyInfo.address}</p>
             </div>
           </div>
-          <div>
-            <h3 className='mb-4 font-bold'>Company info</h3>
+          <div className='hidden pl-20 md:block'>
+            <h3 className='mb-4 font-bold'>Company Info</h3>
             <ul className='space-y-2 text-sm'>
               <li>
                 <Link href='/about'>About Kyndly</Link>
@@ -74,8 +74,8 @@ const Footer: React.FC<FooterProps> = ({
               </li>
             </ul>
           </div>
-          <div>
-            <h3 className='mb-4 font-bold'>Pages</h3>
+          <div className='hidden pl-24 md:block'>
+            <h3 className='mb-4  font-bold'>Pages</h3>
             <ul className='space-y-2 text-sm'>
               <li>
                 <Link href='/search'>Browse Items</Link>
@@ -91,7 +91,7 @@ const Footer: React.FC<FooterProps> = ({
               </li>
             </ul>
           </div>
-          <div>
+          <div className='hidden pl-12 md:block'>
             <h3 className='mb-4 font-bold'>Additional</h3>
             <ul className='space-y-2 text-sm'>
               <li>
@@ -103,55 +103,10 @@ const Footer: React.FC<FooterProps> = ({
             </ul>
           </div>
         </div>
-        <div className='md:hidden'>
-          <h3 className='mb-4 font-medium'>Follow us at:</h3>
-          <div className='flex space-x-4'>
-            {socialLinks.facebook && (
-              <Link
-                href={socialLinks.facebook}
-                className='text-gray-600 hover:text-gray-900'
-              >
-                <FacebookIcon className='h-6 w-6' />
-              </Link>
-            )}
-            {socialLinks.instagram && (
-              <Link
-                href={socialLinks.instagram}
-                className='text-gray-600 hover:text-gray-900'
-              >
-                <InstagramIcon className='h-6 w-6' />
-              </Link>
-            )}
-            {socialLinks.threads && (
-              <Link
-                href={socialLinks.threads}
-                className='text-gray-600 hover:text-gray-900'
-              >
-                <ThreadsIcon className='h-6 w-6' />
-              </Link>
-            )}
-          </div>
-          <div className='mt-4 space-y-2 text-sm'>
-            <Link href='/privacy' className='block'>
-              Privacy Policy
-            </Link>
-            <Link href='/terms' className='block'>
-              Terms and Conditions
-            </Link>
-          </div>
-          <div className='mt-4'>
-            <p className='font-medium'>{companyInfo.name}</p>
-            <p className='text-sm'>
-              UK Registered Company Number {companyInfo.registrationNumber}
-            </p>
-            <p className='mt-2 text-sm'>Registered Address</p>
-            <p className='text-sm'>{companyInfo.address}</p>
-          </div>
-        </div>
       </div>
       <div className='h-8 border-t border-gray-200 bg-primaryOrange py-2'>
         <div className='mx-auto max-w-7xl px-4'>
-          <p className='text-center text-sm leading-[14px] text-white'>
+          <p className='text-center text-sm text-white'>
             ©2024 {companyInfo.name}
           </p>
         </div>
