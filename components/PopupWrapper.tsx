318192aa1d6dd2da946d59fc059fa42fc51949ee
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2025-03-21 21:45:46 +0000

    style: add backdrop blur according to figma design relates #407

--- components/PopupWrapper.tsx
+++ components/PopupWrapper.tsx
@@ -29,17 +29,21 @@ export default function PopupWrapper({ children }: { children: ReactNode }) {
     -translate-x-[50%]
     top-[50%]
     -translate-y-[50%]
+    z-30
   `;
 
   return (
-    <div className={wrapperStyling}>
-      <button
-        type='button'
-        className='align-self-start h-5px w-5px right-[1px] top-[3px] z-30 -mb-[10px] -ml-[10px] cursor-pointer justify-self-end'
-      >
-        x
-      </button>
-      {children}
-    </div>
+    <>
+      <div className='absolute left-0 top-0 z-20 m-0 h-screen w-screen bg-primaryGray opacity-[75%]' />
+      <div className={wrapperStyling}>
+        <button
+          type='button'
+          className='align-self-start right-[1px] top-[3px] z-30 -mb-[10px] -ml-[10px] cursor-pointer justify-self-end'
+        >
+          x
+        </button>
+        {children}
+      </div>
+    </>
   );
 }
