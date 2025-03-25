d1baf769a244db0a85f3c0b7923a88696ef0cc9f
Nich Galzin <nichgalzin@gmail.com>
2025-03-25 14:33:56 +0000

    fix: fixes data return type
    Relates #399

--- app/(dashboard)/item/[id]/page.tsx
+++ app/(dashboard)/item/[id]/page.tsx
@@ -9,7 +9,7 @@ const DisplayItemDetails = async ({ params }: { params: { id: string } }) => {
 
   const supabase = newServerClient();
 
-  const { data: userProfile } = await getProfile(userId);
+  const userProfile = await getProfile(userId);
   let canMessage: boolean = true;
 
   try {
