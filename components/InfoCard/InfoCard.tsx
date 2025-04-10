c59dce7a7acca3552fa65809f9cdd875ae765cc6
Olena Reukova <34659641+OlenaReukova@users.noreply.github.com>
2025-04-10 22:31:32 +0100

    style: desktop infoCard component

--- components/InfoCard/InfoCard.tsx
+++ components/InfoCard/InfoCard.tsx
@@ -7,7 +7,7 @@ interface InfoCardProps {
 
 const InfoCard: React.FC<InfoCardProps> = ({ Icon, title }) => {
   return (
-    <div className='flex w-full flex-col items-center rounded-lg bg-orange-100 p-6 text-center shadow-md sm:w-64'>
+    <div className='flex h-[255px] w-full max-w-[255px]  flex-col items-center justify-center rounded-lg bg-orange-100 p-6 text-center shadow-md'>
       <Icon width={64} height={64} />
       <p className='mt-4 font-medium text-gray-800'>{title}</p>
     </div>
--- components/InfoCard/InfoCards.tsx
+++ components/InfoCard/InfoCards.tsx
@@ -14,10 +14,25 @@ const cardData = [
 
 const InfoCards: React.FC = () => {
   return (
-    <div className='xs:grid-cols-2 mx-auto grid max-w-5xl grid-cols-2 gap-4 p-4 sm:gap-6 sm:p-6 md:grid-cols-2 lg:grid-cols-4'>
-      {cardData.map((card, index) => (
-        <InfoCard key={index} Icon={card.Icon} title={card.title} />
-      ))}
+    <div className='mx-auto flex justify-center sm:w-[80%] lg:max-w-[1080px]'>
+      <div className='my-[98px] flex w-full flex-col items-center'>
+        <div className='mb-16 flex w-full flex-col items-center text-center'>
+          <h1 className='mb-6 text-[56px] font-medium text-gray-800'>
+            Welcome to KINDLY - Where
+            <br />
+            Compassion Meets Sustainability
+          </h1>
+          <h5 className='text-[24px] text-gray-700'>
+            Helping Ukrainian refugees in the UK through donated clothes and
+            personal items.
+          </h5>
+        </div>
+        <div className='sm:justify-content-center flex  w-full flex-wrap justify-between gap-y-6 sm:gap-4'>
+          {cardData.map((card, index) => (
+            <InfoCard key={index} Icon={card.Icon} title={card.title} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
