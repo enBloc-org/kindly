05a8899476e32b94ec8494a9e3a5df01e6c56baf
Olena Reukova <34659641+OlenaReukova@users.noreply.github.com>
2025-04-11 11:41:02 +0100

    style:mobile InfoCards responsiveness

--- components/InfoCard/InfoCard.tsx
+++ components/InfoCard/InfoCard.tsx
@@ -7,9 +7,11 @@ interface InfoCardProps {
 
 const InfoCard: React.FC<InfoCardProps> = ({ Icon, title }) => {
   return (
-    <div className='flex h-[255px] w-full max-w-[255px]  flex-col items-center justify-center rounded-lg bg-orange-100 p-6 text-center shadow-md'>
+    <div className='flex h-[171px] w-full max-w-[171px] flex-col items-center  justify-center rounded-lg bg-orange-100  text-center shadow-md lg:h-[255px] lg:max-w-[255px]'>
       <Icon width={64} height={64} />
-      <p className='mt-4 font-medium text-gray-800'>{title}</p>
+      <p className='mt-4 text-[14px] font-medium text-gray-800 lg:text-[20px]'>
+        {title}
+      </p>
     </div>
   );
 };
--- components/InfoCard/InfoCards.tsx
+++ components/InfoCard/InfoCards.tsx
@@ -14,20 +14,18 @@ const cardData = [
 
 const InfoCards: React.FC = () => {
   return (
-    <div className='mx-auto flex justify-center sm:w-[80%] lg:max-w-[1080px]'>
-      <div className='my-[98px] flex w-full flex-col items-center'>
+    <div className='mx-auto flex w-[90%] justify-center  lg:max-w-[1080px]'>
+      <div className='my-[40px] flex w-full flex-col items-center lg:my-[96px]'>
         <div className='mb-16 flex w-full flex-col items-center text-center'>
-          <h1 className='mb-6 text-[56px] font-medium text-gray-800'>
-            Welcome to KINDLY - Where
-            <br />
-            Compassion Meets Sustainability
+          <h1 className='mb-6 px-10 text-[28px] font-[600] text-gray-800 md:text-[36px] lg:text-[56px] lg:font-medium'>
+            Welcome to KINDLY - Where Compassion Meets Sustainability
           </h1>
-          <h5 className='text-[24px] text-gray-700'>
+          <h5 className='px-4 text-[18px] text-gray-700 md:text-[20px] lg:text-[24px] '>
             Helping Ukrainian refugees in the UK through donated clothes and
             personal items.
           </h5>
         </div>
-        <div className='sm:justify-content-center flex  w-full flex-wrap justify-between gap-y-6 sm:gap-4'>
+        <div className=' flex  w-full flex-wrap  justify-around gap-y-2 lg:justify-between lg:gap-y-6'>
           {cardData.map((card, index) => (
             <InfoCard key={index} Icon={card.Icon} title={card.title} />
           ))}
