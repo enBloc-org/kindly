3c0eb7e5d077e922964e8119cfafeb63360514f4
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2025-06-15 16:58:56 +0100

    style: round corners dynamically with css logical properties
    relates #423

--- components/messaging/MessageCard.tsx
+++ components/messaging/MessageCard.tsx
@@ -30,6 +30,7 @@ const MessageCard: React.FC<MessageCardProps> = ({
 
   return (
     <div
+      dir={`${isCurrentUser ? 'ltr' : 'rtl'}`}
       className={`message-card ${isCurrentUser ? 'float-right bg-secondaryGray lg:left-24' : 'float-left bg-secondaryGreen lg:right-10'} justify-content my-2 flex flex-col-reverse`}
       ref={messageRef}
     >
