04c773ac071d45f19f10c40fb66c6cb971b9fd36
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2025-06-15 16:54:37 +0100

    style: add user name to messagecard display
    relates #423

--- components/messaging/MessageCard.tsx
+++ components/messaging/MessageCard.tsx
@@ -30,17 +30,20 @@ const MessageCard: React.FC<MessageCardProps> = ({
 
   return (
     <div
-      className={`message-card ${isCurrentUser ? 'float-right bg-secondaryGray lg:left-24' : 'float-left bg-secondaryGreen lg:right-10'} my-2`}
+      className={`message-card ${isCurrentUser ? 'float-right bg-secondaryGray lg:left-24' : 'float-left bg-secondaryGreen lg:right-10'} justify-content my-2 flex flex-col-reverse`}
       ref={messageRef}
     >
-      <div className='mx-2 flex items-center justify-center'>
+      <div className='flex w-full items-center justify-center'>
         <p
-          className={`px-2 py-2 text-lg ${messageText.length > 50 && 'md:px-4'} whitespace-pre-wrap lg:ml-10`}
+          className={`px-0 text-sm ${messageText.length > 50 && 'md:px-4'} w-full whitespace-pre-wrap text-start lg:ml-10`}
         >
           {messageText}
         </p>
       </div>
-      <div className={`${isCurrentUser && 'lg:mr-24'}`}>
+      <div
+        className={`${isCurrentUser && 'lg:mr-24'} align-center flex w-full flex-row justify-between`}
+      >
+        <p className='text-sm font-bold'>{`${isCurrentUser ? 'You' : currentUser}`}</p>
         <p className='text-sm font-light lg:text-base'>{createdAt}</p>
       </div>
     </div>
