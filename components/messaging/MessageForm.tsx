b456a10dcb76a7f3890f53bc6ecde5c55c0b5c84
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2025-06-22 20:55:19 +0100

    style: set bottom margin
    relates #423

--- components/messaging/MessageForm.tsx
+++ components/messaging/MessageForm.tsx
@@ -100,7 +100,7 @@ const MessageForm: React.FC<MessageFormProps> = ({
       onSubmit={handleSubmit}
       className='z-50 flex items-center justify-center gap-6 p-4'
     >
-      <div className='relative h-[65px] w-5/6'>
+      <div className='relative mb-[10px] h-[65px] w-full'>
         <textarea
           className='h-full w-full resize-none overflow-hidden rounded-lg
         bg-secondaryGray px-4 py-2 pt-5 text-black'
