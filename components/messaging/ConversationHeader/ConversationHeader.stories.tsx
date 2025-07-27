f8b13799c260e231bb0cd554c4e3685ba71f5f65
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2025-07-27 18:00:10 +0100

    fix: remove redundant test
    relates #430

--- components/messaging/ConversationHeader/ConversationHeader.stories.tsx
+++ components/messaging/ConversationHeader/ConversationHeader.stories.tsx
@@ -1,7 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import ConversationHeader from './ConversationHeader';
-import { within, userEvent, waitFor } from '@storybook/test';
-import { expect } from '@storybook/jest';
 import ConversationContextProvider from '@/context/conversationContext';
 
 const meta: Meta<typeof ConversationHeader> = {
@@ -22,12 +20,5 @@ type Story = StoryObj<typeof meta>;
 export const DefaultConversationHeader: Story = {
   args: {
     partnerName: 'Maria Melnyk',
-  },
-  play: async ({ canvasElement, step }) => {
-    const canvas = within(canvasElement);
-    await step('Open ellipsis menu', async () => {
-      await userEvent.click(canvas.getByRole('button'));
-    });
-    await waitFor(() => expect(canvas.getByText(/chat rules/gi)).toBeVisible());
   },
 };
