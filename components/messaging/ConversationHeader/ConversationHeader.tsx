bcf13a888f5c82cd9820f3e7cf4da2a48925c34b
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2025-07-27 16:08:14 +0100

    feat: import context to allow toggling back to conversationslist display
    relates #430

--- components/messaging/ConversationHeader/ConversationHeader.tsx
+++ components/messaging/ConversationHeader/ConversationHeader.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import BackButton from '@/components/buttons/BackButton';
 import EllipsisMenu from '@/components/menus/EllipsisMenu';
+import { useConversationContext } from '@/context/conversationContext';
 
 export default function ConversationHeader({
   partnerName,
 }: {
   partnerName: string;
 }) {
+  const { dispatch } = useConversationContext();
+
   const conversationOptions = [
     {
       buttonMessage: 'do things',
@@ -20,9 +22,33 @@ export default function ConversationHeader({
         console.log(`clicked at ${e.timeStamp}`),
     },
   ];
+
+  const toggleDisplay = () => {
+    dispatch({
+      type: 'SET_SHOW_CONVERSATIONS_LIST',
+      payload: true,
+    });
+  };
+
   return (
     <div className='flex items-center justify-between p-2'>
-      <BackButton />
+      <button onClick={toggleDisplay}>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='40'
+          height='40'
+          viewBox='0 0 512 512'
+        >
+          <path
+            fill='none'
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth='48'
+            d='M244 400L100 256l144-144M120 256h292'
+          />
+        </svg>
+      </button>
       <p className='text-xl font-bold'>{partnerName}</p>
       <EllipsisMenu menuOptions={conversationOptions} />
     </div>
